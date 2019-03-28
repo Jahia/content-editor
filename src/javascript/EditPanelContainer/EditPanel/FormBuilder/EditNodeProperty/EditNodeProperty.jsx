509b3c5bdb83613b14020c622de10f24a7a47620
Taybou <benterki.tayeb@gmail.com>
2019-03-28 18:14:26 +0100

    BACKLOG-9954 replace "Chip" with Badge and style component

--- src/javascript/EditPanelContainer/EditPanel/FormBuilder/EditNodeProperty/EditNodeProperty.jsx
+++ src/javascript/EditPanelContainer/EditPanel/FormBuilder/EditNodeProperty/EditNodeProperty.jsx
@@ -1,35 +1,70 @@
 import React from 'react';
-import {FormControl, InputLabel, withStyles} from '@material-ui/core';
+import {FormControl, InputLabel, withStyles, Grid} from '@material-ui/core';
 import {MoreVert, Public} from '@material-ui/icons';
-import {Button, Chip} from '@jahia/ds-mui-theme';
+import {Button, Badge} from '@jahia/ds-mui-theme';
 import {compose} from 'react-apollo';
 import {translate} from 'react-i18next';
 import * as PropTypes from 'prop-types';
 
 let styles = theme => ({
     formControl: Object.assign(theme.typography.zeta, {
-        width: '100%',
         padding: '16px 0',
-        display: 'flex',
-        flexDirection: 'row'
+        flexGrow: 1
     }),
     inputLabel: {
         color: theme.palette.font.beta
+    },
+    input: {
+        flexGrow: 5
+    },
+    badge: {
+        marginBottom: theme.spacing.unit,
+        marginRight: 84
     }
 });
 
 export const EditNodeProperty = ({t, classes, children, field, siteInfo}) => {
     return (
         <FormControl className={classes.formControl}>
-            <InputLabel shrink className={classes.inputLabel}>
-                {field.formDefinition.name}
-            </InputLabel>
+            <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+            >
+                <Grid item>
+                    <InputLabel shrink
+                                className={classes.inputLabel}
+                                style={(!field.formDefinition.i18n && siteInfo.languages.length > 1) ? {paddingTop: 32} : {}}
+                    >
+                        {field.formDefinition.name}
+                    </InputLabel>
+                </Grid>
 
-            {(!field.formDefinition.i18n && siteInfo.languages.length > 1) &&
-                <Chip icon={<Public/>} label={t('content-editor:label.contentEditor.edit.sharedLanguages')} size="compact"/>}
+                {(!field.formDefinition.i18n && siteInfo.languages.length > 1) &&
+                <Grid item>
+                    <Badge className={classes.badge}
+                           badgeContent={t('content-editor:label.contentEditor.edit.sharedLanguages')}
+                           icon={<Public/>}
+                           variant="normal"
+                           color="warning"
+                    />
+                </Grid>
+                }
+            </Grid>
 
-            {children}
-            <Button><MoreVert/></Button>
+            <Grid
+                container
+                direction="row"
+                alignItems="center"
+            >
+                <Grid item className={classes.input}>
+                    {children}
+                </Grid>
+                <Grid item>
+                    <Button variant="iconGhost" icon={<MoreVert/>}/>
+                </Grid>
+            </Grid>
         </FormControl>
     );
 };
@@ -46,4 +81,3 @@ export default compose(
     translate(),
     withStyles(styles)
 )(EditNodeProperty);
-
