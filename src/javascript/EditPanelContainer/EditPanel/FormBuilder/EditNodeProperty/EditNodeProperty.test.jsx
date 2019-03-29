5c503fac57c11031159892885ae7010351057adc
Taybou <benterki.tayeb@gmail.com>
2019-03-29 10:35:20 +0100

    BACKLOG-9954 update unit test to use "Badge" instead of "Chip"

--- src/javascript/EditPanelContainer/EditPanel/FormBuilder/EditNodeProperty/EditNodeProperty.test.jsx
+++ src/javascript/EditPanelContainer/EditPanel/FormBuilder/EditNodeProperty/EditNodeProperty.test.jsx
@@ -21,13 +21,13 @@ describe('EditNodeProperty component', () => {
         let lang1 = {displayName: 'Deutsch', language: 'de', activeInEdit: true};
         let lang2 = {displayName: 'English', language: 'en', activeInEdit: true};
 
-        testI18nChipRender(false, [lang1, lang2], true);
-        testI18nChipRender(true, [lang1, lang2], false);
-        testI18nChipRender(false, [lang1], false);
-        testI18nChipRender(true, [lang1], false);
+        testI18nBadgeRender(false, [lang1, lang2], true);
+        testI18nBadgeRender(true, [lang1, lang2], false);
+        testI18nBadgeRender(false, [lang1], false);
+        testI18nBadgeRender(true, [lang1], false);
     });
 
-    let testI18nChipRender = function (i18n, siteLanguages, expectedChipRendered) {
+    let testI18nBadgeRender = function (i18n, siteLanguages, expectedBadgeRendered) {
         props.field =
             {formDefinition: {name: 'x', selectorType: 'Text', i18n: i18n},
                 targets: [{name: 'test'}]};
@@ -37,7 +37,7 @@ describe('EditNodeProperty component', () => {
 
         wrapper.setProps(props);
 
-        const chipComponent = wrapper.find({label: 'content-editor:label.contentEditor.edit.sharedLanguages'});
-        expect(chipComponent.exists()).toBe(expectedChipRendered);
+        const badgeComponent = wrapper.find({badgeContent: 'content-editor:label.contentEditor.edit.sharedLanguages'});
+        expect(badgeComponent.exists()).toBe(expectedBadgeRendered);
     };
 });
