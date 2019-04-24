2b5b9aae6ffc01fa9638273be5f39e3a9ff94c24
Kevan <kevan@jahia.com>
2019-04-24 15:37:44 +0200

    BACKLOG-10099: fix branch rebase with master

--- src/javascript/EditPanelContainer/EditPanelContainer.jsx
+++ src/javascript/EditPanelContainer/EditPanelContainer.jsx
@@ -105,6 +105,7 @@ export const EditPanelContainer = ({
                                                                 nodeData={
                                                                     nodeData
                                                                 }
+                                                                language={lang}
                                                             />
                                                         );
                                                     }}
