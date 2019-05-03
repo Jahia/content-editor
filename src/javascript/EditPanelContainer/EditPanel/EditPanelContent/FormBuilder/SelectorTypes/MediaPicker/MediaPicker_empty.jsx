2e90a905f9983c128f2c812c97a34151a436051e
David Griffon <dgriffon@jahia.com>
2019-05-03 12:35:03 +0200

    BACKLOG-10209 : resolve conflict rebasing image picker feature branch onto master

--- src/javascript/EditPanelContainer/EditPanel/EditPanelContent/FormBuilder/SelectorTypes/MediaPicker/MediaPicker_empty.jsx
+++ src/javascript/EditPanelContainer/EditPanel/EditPanelContent/FormBuilder/SelectorTypes/MediaPicker/MediaPicker_empty.jsx
@@ -37,11 +37,7 @@ function Transition(props) {
     return <Slide direction="up" {...props}/>;
 }
 
-<<<<<<< HEAD:src/javascript/EditPanelContainer/EditPanel/EditPanelContent/FormBuilder/SelectorTypes/MediaPicker/MediaPicker_empty.jsx
 export const MediaPickerEmptyCmp = ({classes, t, idInput, editorContext, onImageSelection}) => {
-=======
-export const MediaPickerEmptyCmp = ({classes, t, idInput, editorContext}) => {
->>>>>>> 59c442b... BACKLOG-10146: rename context to editorContext:src/javascript/EditPanelContainer/EditPanel/EditPanelContent/FormBuilder/SelectorTypes/mediaPicker/mediaPicker_empty.js
     const [isOpen, setIsOpen] = useState(false);
 
     return (
