6762056a6f3387c6d846f3a41230f541d7b0bb04
Taybou <benterki.tayeb@gmail.com>
2019-05-23 12:47:37 +0200

    BACKLOG-10270 add ContentTable component to design system in CE

---
+++ src/javascript/DesignSystem/ContentTable/ContentTable.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '@material-ui/core/Checkbox';
+import {ContentTableHeader} from './ContentTableHeader';
+import {EmptyTable} from './EmptyTable';
+
+const styles = theme => ({
+    tableWrapper: {
+        flex: '1 1 0%',
+        overflow: 'auto',
+        position: 'relative',
+        marginTop: theme.spacing.unit * 3,
+        marginBottom: theme.spacing.unit * 9
+    },
+    table: {
+        height: '100%'
+    },
+    row: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default + '7F'
+        },
+        '&:hover': {
+            backgroundColor: theme.palette.background.default
+        }
+    }
+});
+
+const ContentTable = ({data, order, orderBy, columns, labelEmpty, classes}) => {
+    return (
+        <div className={classes.tableWrapper}>
+            <Table className={classes.table} aria-labelledby="tableTitle">
+                <ContentTableHeader
+                    columns={columns}
+                    order={order}
+                    orderBy={orderBy}
+                />
+                {data && data.length === 0 ?
+                    <EmptyTable labelEmpty={labelEmpty}/> :
+                    <TableBody>
+                        {data.map(row => {
+                            return (
+                                <TableRow key={row.id}
+                                          hover
+                                          className={classes.row}
+                                          role="checkbox"
+                                          selected={false}
+                                          tabIndex={-1}
+                                          onClick={() => {
+                                              // TODO: handle selection (manage selected & checked)
+                                          }}
+                                >
+                                    <TableCell padding="checkbox">
+                                        <Checkbox checked={false}/>
+                                    </TableCell>
+
+                                    {columns.map(column => {
+                                        return (
+                                            <TableCell key={row.id}>{row[column.property]}</TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                }
+            </Table>
+        </div>
+    );
+};
+
+ContentTable.propTypes = {
+    data: PropTypes.PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        createdBy: PropTypes.string.isRequired,
+        lastModified: PropTypes.string.isRequired
+    })).isRequired,
+    columns: PropTypes.array.isRequired,
+    order: PropTypes.string.isRequired,
+    orderBy: PropTypes.string.isRequired,
+    labelEmpty: PropTypes.string.isRequired,
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ContentTable);
---
+++ src/javascript/DesignSystem/ContentTable/ContentTable.spec.js
@@ -0,0 +1,82 @@
+import {dsGenericTheme} from '@jahia/ds-mui-theme';
+import {shallowWithTheme} from '@jahia/test-framework';
+import React from 'react';
+import ContentTable from './ContentTable';
+
+describe('ContentTable', () => {
+    let defaultProps;
+
+    beforeEach(() => {
+        defaultProps = {
+            data: [
+                {id: '1', name: 'name 1', type: 'type 1', createdBy: 'createdBy 1', lastModified: 'lastModified 1'},
+                {id: '2', name: 'name 2', type: 'type 2', createdBy: 'createdBy 2', lastModified: 'lastModified 2'}
+            ],
+            columns: [
+                {property: 'name', label: 'Column 1'},
+                {property: 'type', label: 'Column 2'},
+                {property: 'createdBy', label: 'Column 3'},
+                {property: 'lastModified', label: 'Column 4'}
+            ],
+            order: '',
+            orderBy: '',
+            labelEmpty: '',
+            classes: {}
+        };
+    });
+
+    it('should display ContentTable when data is not empty', () => {
+        const cmp = shallowWithTheme(
+            <ContentTable {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        cmp.find('WithStyles(Table)').exists();
+    });
+
+    it('should display EmptyTable when data is empty', () => {
+        defaultProps.data = [];
+        const cmp = shallowWithTheme(
+            <ContentTable {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        cmp.find('WithStyles(EmptyTable)').exists();
+    });
+
+    it('should display ContentTableHeader inside the table', () => {
+        const cmp = shallowWithTheme(
+            <ContentTable {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        cmp.find('ContentTableHeader').exists();
+    });
+
+    it('should display table with rows', () => {
+        const cmp = shallowWithTheme(
+            <ContentTable {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        expect(cmp.find('WithStyles(TableRow)').debug()).toContain('name 1');
+        expect(cmp.find('WithStyles(TableRow)').debug()).toContain('name 2');
+    });
+
+    it('should display table row', () => {
+        const cmp = shallowWithTheme(
+            <ContentTable {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        expect(cmp.find('WithStyles(TableRow)').debug()).toContain('name 1');
+        expect(cmp.find('WithStyles(TableRow)').debug()).toContain('type 1');
+        expect(cmp.find('WithStyles(TableRow)').debug()).toContain('createdBy 1');
+        expect(cmp.find('WithStyles(TableRow)').debug()).toContain('lastModified 1');
+    });
+});
