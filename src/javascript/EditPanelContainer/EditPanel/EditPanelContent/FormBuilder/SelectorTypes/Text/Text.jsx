b23c44bcc8d281591dd5c9dfce28d1eeda8a9576
Taybou <benterki.tayeb@gmail.com>
2019-06-24 14:52:35 +0200

    BACKLOG-10484 use content editor UI language to resolve decimalSeparator

--- src/javascript/EditPanelContainer/EditPanel/EditPanelContent/FormBuilder/SelectorTypes/Text/Text.jsx
+++ src/javascript/EditPanelContainer/EditPanel/EditPanelContent/FormBuilder/SelectorTypes/Text/Text.jsx
@@ -4,35 +4,31 @@ import {connect} from 'formik';
 import PropTypes from 'prop-types';
 import {Input} from '../../../../../../DesignSystem/Input';
 
-export class Text extends React.Component {
-    render() {
-        const {field, id} = this.props;
-        const {values, handleChange, handleBlur} = this.props.formik;
+export const Text = ({field, id, editorContext, formik: {values, handleChange, handleBlur}}) => {
+    const fieldType = field.jcrDefinition.requiredType;
+    const isNumber = fieldType === 'DOUBLE' || fieldType === 'LONG' || fieldType === 'DECIMAL';
+    const decimalSeparator = editorContext.uiLang === 'en' ? '.' : ',';
+    const value = values[field.formDefinition.name];
 
-        const fieldType = field.jcrDefinition.requiredType;
-        const isNumber = fieldType === 'DOUBLE' || fieldType === 'LONG' || fieldType === 'DECIMAL';
-        const decimalSeparator = navigator.language.includes('en') ? '.' : ',';
-        const value = values[field.formDefinition.name];
-
-        return (
-            <Input
-                fullWidth
-                id={id}
-                name={field.formDefinition.name}
-                defaultValue={isNumber ? value && value.replace('.', decimalSeparator) : value}
-                readOnly={field.formDefinition.readOnly}
-                type={isNumber ? 'number' : 'text'}
-                decimalScale={fieldType === 'LONG' ? 0 : undefined}
-                decimalSeparator={decimalSeparator}
-                onChange={handleChange}
-                onBlur={handleBlur}
-            />
-        );
-    }
-}
+    return (
+        <Input
+            fullWidth
+            id={id}
+            name={field.formDefinition.name}
+            defaultValue={isNumber ? value && value.replace('.', decimalSeparator) : value}
+            readOnly={field.formDefinition.readOnly}
+            type={isNumber ? 'number' : 'text'}
+            decimalScale={fieldType === 'LONG' ? 0 : undefined}
+            decimalSeparator={decimalSeparator}
+            onChange={handleChange}
+            onBlur={handleBlur}
+        />
+    );
+};
 
 Text.propTypes = {
     id: PropTypes.string.isRequired,
+    editorContext: PropTypes.object.isRequired,
     field: PropTypes.object.isRequired,
     formik: PropTypes.object.isRequired
 };
