26fc376531e9e84230ae2a6299a30c92ef3b2b25
Taybou <benterki.tayeb@gmail.com>
2019-07-05 14:48:07 +0200

    BACKLOG-10542 add react unit tests

--- src/javascript/EditPanelContainer/EditPanel/EditPanelDialogConfirmation/EditPanelDialogConfirmation.spec.jsx
+++ src/javascript/EditPanelContainer/EditPanel/EditPanelDialogConfirmation/EditPanelDialogConfirmation.spec.jsx
@@ -17,12 +17,47 @@ describe('EditPanelDialogConfirmation', () => {
         };
     });
 
-    // TODO BACKLOG-10542
-    it('should not', () => {
-        shallowWithTheme(
+    it('should hide dialog confirmation when open is false', () => {
+        const cmp = shallowWithTheme(
             <EditPanelDialogConfirmation {...defaultProps}/>,
             {},
             dsGenericTheme
-        );
+        ).dive();
+
+        expect(cmp.props().open).toBe(false);
+    });
+
+    it('should show dialog confirmation when open is true', () => {
+        defaultProps.open = true;
+
+        const cmp = shallowWithTheme(
+            <EditPanelDialogConfirmation {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        expect(cmp.props().open).toBe(true);
+    });
+
+    it('should display cancel and save buttons in dialog confirmation when it is not allowed to discard', () => {
+        const cmp = shallowWithTheme(
+            <EditPanelDialogConfirmation {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        expect(cmp.find('WithStyles(Button)').length).toBe(2);
+    });
+
+    it('should display cancel, discard and save buttons in dialog confirmation when it is allowed to discard', () => {
+        defaultProps.allowDiscard = true;
+
+        const cmp = shallowWithTheme(
+            <EditPanelDialogConfirmation {...defaultProps}/>,
+            {},
+            dsGenericTheme
+        ).dive();
+
+        expect(cmp.find('WithStyles(Button)').length).toBe(3);
     });
 });
