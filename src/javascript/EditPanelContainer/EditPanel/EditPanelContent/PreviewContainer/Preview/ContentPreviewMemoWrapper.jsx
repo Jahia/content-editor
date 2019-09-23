b7264b4c861aa1f622a67788e9b52904747f037b
Kevan <kevan@jahia.com>
2019-09-23 11:26:34 +0200

    BACKLOG-11113: zoom on content during content preview

--- src/javascript/EditPanelContainer/EditPanel/EditPanelContent/PreviewContainer/Preview/ContentPreviewMemoWrapper.jsx
+++ src/javascript/EditPanelContainer/EditPanel/EditPanelContent/PreviewContainer/Preview/ContentPreviewMemoWrapper.jsx
@@ -3,18 +3,32 @@ import {ContentPreview} from '@jahia/react-apollo';
 import {setPreviewRefetcher} from '../../../../EditPanel.refetches';
 import {PreviewComponent} from '@jahia/react-material';
 import {useContentEditorContext} from '../../../../../ContentEditor.context';
-import {getPreviewContext} from './Preview.utils';
+import {getPreviewContext, removeSiblings} from './Preview.utils';
 
 export const ContentPreviewMemoWrapper = React.memo(() => {
     const editorContext = useContentEditorContext();
     const previewContext = getPreviewContext(editorContext);
+
+    const domLoadedCallback = frameDoc => {
+        // No zoom on full if no content wrapped in the page
+        if (!previewContext.requestAttributes) {
+            return;
+        }
+
+        const contentPreview = frameDoc.getElementById('ce_preview_content');
+        if (contentPreview) {
+            removeSiblings(contentPreview);
+        } else {
+            // Todo display badge content not found
+        }
+    };
 
     return (
         <ContentPreview {...previewContext}
                         fetchPolicy="network-only"
                         setRefetch={refetchingData => setPreviewRefetcher(refetchingData)}
         >
-            {data => <PreviewComponent data={data.jcr ? data.jcr : {}} workspace={previewContext.workspace}/>}
+            {data => <PreviewComponent data={data.jcr ? data.jcr : {}} workspace={previewContext.workspace} domLoadedCallback={domLoadedCallback}/>}
         </ContentPreview>
     );
 });
