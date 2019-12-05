5c299e0c08876d316c0fbe9f2c9726ac3dcc3e4c
Kevan <kevan@jahia.com>
2019-12-05 16:22:02 +0100

    BACKLOG-11573: use thumbnail for meedias in media picker

--- src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Picker/MediaPicker/ImageListQuery/ImageListQuery.jsx
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Picker/MediaPicker/ImageListQuery/ImageListQuery.jsx
@@ -46,7 +46,7 @@ const ImageListQueryCmp = ({
             uuid: rawImg.uuid,
             url: `${
                 window.contextJsParameters.contextPath
-            }/files/default${encodeJCRPath(rawImg.path)}?lastModified=${rawImg.lastModified.value}`,
+            }/files/default${encodeJCRPath(rawImg.path)}?lastModified=${rawImg.lastModified.value}&t=thumbnail2`,
             path: rawImg.path,
             name: rawImg.name,
             type: rawImg.children.nodes[0].mimeType.value.replace('image/', ''),
