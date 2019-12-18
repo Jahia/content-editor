f0f6ff86040a6a2ae542799ba71dece1cf6ec0f2
David Griffon <dgriffon@jahia.com>
2019-12-18 15:37:08 +0100

    BACKLOG-11935 : fix display of tabs from custom configuration

--- src/javascript/Edit/engineTabs/engineTabs.utils.js
+++ src/javascript/Edit/engineTabs/engineTabs.utils.js
@@ -3,6 +3,17 @@ import openEngineTabs from './openEngineTabs.action';
 import {Constants} from '~/ContentEditor.constants';
 
 /**
+ * There is no test related to those functions as they are supposed to be deprecated soon.
+ * Also as they are mostly related to external call (GWT) more of the logic is outside this code.
+ */
+
+const getNodeTypes = primaryNodeType => {
+    let nodeTypes = primaryNodeType.supertypes.map(nodeType => nodeType.name);
+    nodeTypes.unshift(primaryNodeType.name);
+    return nodeTypes;
+};
+
+/**
  * This function open the edit engine tabs of to the node
  * that has been specified in the method parameters
  *
@@ -16,7 +27,7 @@ export function openEngineTab(nodeData, engineTabs) {
         path,
         displayName,
         mixinTypes.map(mixinType => mixinType.name),
-        primaryNodeType.supertypes.map(nodeType => nodeType.name),
+        getNodeTypes(primaryNodeType),
         uuid,
         false,
         {
@@ -41,15 +52,12 @@ export function registerEngineTabActions(nodeData) {
         return;
     }
 
-    let nodeTypes = primaryNodeType.supertypes.map(nodeType => nodeType.name);
-    nodeTypes.unshift(primaryNodeType.name);
-
     const tabs = window.parent.authoringApi.getEditTabs(
         path,
         uuid,
         displayName,
         mixinTypes.map(mixinType => mixinType.name),
-        nodeTypes,
+        getNodeTypes(primaryNodeType),
         primaryNodeType.hasOrderableChildNodes
     );
 
