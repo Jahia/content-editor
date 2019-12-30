705edbf80e1dc0d0f3ec60e1353e075ad182efe4
Kevan <kevan@jahia.com>
2019-12-30 11:56:56 +0100

    X BACKLOGBACKLOG-10843: hide empty sections

--- src/javascript/EditPanel/FormDefinitions/FormData.adapter.js
+++ src/javascript/EditPanel/FormDefinitions/FormData.adapter.js
@@ -84,19 +84,21 @@ const getTechnicalInfo = (nodeData, t) => {
 const adaptSections = sections => {
     const cloneSections = JSON.parse(JSON.stringify(sections));
 
-    return cloneSections.reduce((result, section) => {
-        if (section.name === 'metadata') {
-            section.fieldSets = section.fieldSets.reduce((fieldSetsField, fieldSet) => {
-                if (fieldSet.fields.find(f => f.readOnly)) {
-                    return [...fieldSetsField];
-                }
+    return cloneSections
+        .reduce((result, section) => {
+            if (section.name === 'metadata') {
+                section.fieldSets = section.fieldSets.reduce((fieldSetsField, fieldSet) => {
+                    if (fieldSet.fields.find(f => f.readOnly)) {
+                        return [...fieldSetsField];
+                    }
 
-                return [...fieldSetsField, fieldSet];
-            }, []);
-        }
+                    return [...fieldSetsField, fieldSet];
+                }, []);
+            }
 
-        return [...result, section];
-    }, []);
+            return [...result, section];
+        }, [])
+        .filter(section => (section.fieldSets && section.fieldSets.length > 0));
 };
 
 const hydrateNodeData = nodeData => {
