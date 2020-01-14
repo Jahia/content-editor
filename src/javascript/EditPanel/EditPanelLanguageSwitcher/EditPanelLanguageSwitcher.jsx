2bbfe409f195feb07f4629dd59d971a21f324a46
Kevan <kevan@jahia.com>
2020-01-14 14:40:47 +0100

    BACKLOG-12016: better handling of editorId

--- src/javascript/EditPanel/EditPanelLanguageSwitcher/EditPanelLanguageSwitcher.jsx
+++ src/javascript/EditPanel/EditPanelLanguageSwitcher/EditPanelLanguageSwitcher.jsx
@@ -6,16 +6,11 @@ import {connect} from 'formik';
 import {connect as connectReactRedux} from 'react-redux';
 import {cmGoto} from '~/ContentManager.redux-actions';
 import {EditPanelDialogConfirmation} from '../EditPanelDialogConfirmation';
-import {resetLockEditorId} from '~/Lock/LockUtils';
 
 const EditPanelLanguageSwitcher = ({lang, siteInfo, onSelectLanguage, formik}) => {
     const [dialogConfirmation, setDialogConfirmation] = useState({open: false, lang: lang});
 
     const switchLanguage = language => {
-        // Reset lock id, used because the lock should be refreshed with a new ID,
-        // because the language switcher is not reloading the complete page
-        resetLockEditorId();
-
         // Select callback from CMM
         onSelectLanguage(language);
 
