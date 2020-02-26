8ae10591584e853dada8e5f5035f4a2ae5a1ce6e
Kevan <kevan@jahia.com>
2020-02-26 14:36:00 +0100

    BACKLOG-12535: standalone CE create API

--- src/javascript/Api/ContentEditor.api.jsx
+++ src/javascript/Api/ContentEditor.api.jsx
@@ -4,6 +4,7 @@ import ContentEditor from '~/ContentEditor';
 import {Constants} from '~/ContentEditor.constants';
 import * as PropTypes from 'prop-types';
 import Slide from '@material-ui/core/Slide';
+import {CreateNewContentDialog} from '../Create/CreateNewContentAction/CreateNewContentDialog';
 
 let styles = () => {
     return {
@@ -26,17 +27,37 @@ const ContentEditorApiCmp = ({classes}) => {
     const [create, isCreate] = useState(false);
 
     window.CE_API = window.CE_API || {};
+    /**
+     * Open content editor as a modal to edit the given node
+     * @param path the path of the node to edit
+     * @param site the current site
+     * @param lang the current lang from url
+     * @param uilang the preferred user lang for ui
+     */
     window.CE_API.edit = (path, site, lang, uilang) => {
         isEdit({
             path,
             site,
             lang,
-            uilang
+            uilang,
+            mode: Constants.routes.baseEditRoute
         });
     };
 
-    window.CE_API.create = () => {
-        isCreate(true);
+    /**
+     * Open content type selection then content editor as a modal to create a new content
+     * @param path the parent node path where the content will be created
+     * @param site the current site
+     * @param lang the current lang from url
+     * @param uilang the preferred user lang for ui
+     */
+    window.CE_API.create = (path, site, lang, uilang) => {
+        isCreate({
+            path,
+            site,
+            lang,
+            uilang
+        });
     };
 
     // Standalone env props
@@ -45,18 +66,29 @@ const ContentEditorApiCmp = ({classes}) => {
             isEdit(false);
             isCreate(false);
         },
+        createCallback: createdNodePath => {
+            // Redirect to CE edit mode, for the created node
+            if (edit) {
+                isEdit({
+                    path: createdNodePath,
+                    site: edit.site,
+                    uilang: edit.uilang,
+                    lang: edit.lang,
+                    mode: Constants.routes.baseEditRoute
+                });
+            }
+        },
         setLanguage: lang => {
+            // Update the lang of current opened CE
             if (edit) {
                 isEdit({
                     path: edit.path,
                     site: edit.site,
                     uilang: edit.uilang,
-                    lang: lang
+                    lang: lang,
+                    mode: edit.mode,
+                    contentType: edit.contentType
                 });
-            }
-
-            if (create) {
-                // TODO BACKLOG-12172: handle create
             }
         }
     };
@@ -67,20 +99,40 @@ const ContentEditorApiCmp = ({classes}) => {
             <Dialog fullScreen open TransitionComponent={Transition} aria-labelledby="dialog-content-editor" classes={{root: classes.ceDialogRoot}} onClose={envProps.closeCallback}>
                 <div className={classes.mainBackground}>
                     <ContentEditor env={Constants.env.standalone}
-                                   mode={Constants.routes.baseEditRoute}
+                                   mode={edit.mode}
                                    path={edit.path}
                                    lang={edit.lang}
                                    uilang={edit.uilang}
                                    site={edit.site}
+                                   contentType={edit.contentType}
                                    envProps={envProps}
                     />
                 </div>
             </Dialog>}
 
             {create &&
-            <Dialog open={create} aria-labelledby="dialog-content-editor">
-                <p>create</p>
-            </Dialog>}
+            <CreateNewContentDialog
+                open
+                parentPath={create.path}
+                uilang={create.uilang}
+                onClose={() => {
+                    isCreate(false);
+                }}
+                onExited={() => {
+                    isCreate(false);
+                }}
+                onCreateContent={contentType => {
+                    isCreate(false);
+                    isEdit({
+                        path: create.path,
+                        site: create.site,
+                        uilang: create.uilang,
+                        lang: create.lang,
+                        contentType: contentType.name,
+                        mode: Constants.routes.baseCreateRoute
+                    });
+                }}
+            />}
         </>
     );
 };
