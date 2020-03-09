0fb370d1387199eeda671951284c3e41e4542809
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-03-09 15:17:16 +0100

    BACKLOG-12652 : display detail panel and handle action to switch option

--- src/javascript/EditPanel/AdvancedOptions/AdvancedOptions.jsx
+++ src/javascript/EditPanel/AdvancedOptions/AdvancedOptions.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {registry} from '@jahia/ui-extender';
 import {LayoutModule} from '@jahia/moonstone';
 import AdvancedOptionsNavigation from './AdvancedOptionsNavigation/AdvancedOptionsNavigation';
 import {useContentEditorConfigContext} from '~/ContentEditor.context';
 import classes from './AdvancedOptions.scss';
+import {registerAdvancedOptionsActions} from './AdvancedOptions.actions';
+import {Details} from '~/EditPanel/EditPanelContent/PreviewContainer/Details';
+import {useTranslation} from 'react-i18next';
 
 export const AdvancedOptions = () => {
+    const {t} = useTranslation();
     const {nodeData, siteInfo, formik} = useContentEditorConfigContext();
+    registerAdvancedOptionsActions(registry, t);
 
+    const [activeOption, setActiveOption] = useState('technicalInformation');
+    const SelectedTabComponents = {
+        technicalInformation: Details
+    };
+    const SelectedTabComponent = SelectedTabComponents[activeOption];
     return (
         <>
             <div className={classes.container}>
                 <LayoutModule
                     navigation={<AdvancedOptionsNavigation nodeData={nodeData}
                                                            siteInfo={siteInfo}
-                                                           formik={formik}/>}
-                    // TODO BACKLOG-12653 handle action to change view
-                    content={<>temp</>}
+                                                           formik={formik}
+                                                           setActiveOption={setActiveOption}/>}
+                    content={<SelectedTabComponent/>}
                 />
             </div>
         </>
