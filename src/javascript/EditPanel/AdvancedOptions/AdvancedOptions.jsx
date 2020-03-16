5e0d6680c6d1773ad4b2e46d3a915dccf11a4b8d
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-03-16 16:19:33 +0100

    BACKLOG-12783 : refresh correcty detail panel

--- src/javascript/EditPanel/AdvancedOptions/AdvancedOptions.jsx
+++ src/javascript/EditPanel/AdvancedOptions/AdvancedOptions.jsx
@@ -1,18 +1,13 @@
 import React, {useState} from 'react';
-import {registry} from '@jahia/ui-extender';
 import {LayoutModule} from '@jahia/moonstone';
 import AdvancedOptionsNavigation from './AdvancedOptionsNavigation/AdvancedOptionsNavigation';
 import classes from './AdvancedOptions.scss';
-import {registerAdvancedOptionsActions} from './AdvancedOptions.actions';
 import {TechnicalInformation} from './TechnicalInformation/TechnicalInformation';
-import {useTranslation} from 'react-i18next';
 import PropTypes from 'prop-types';
 
 export const AdvancedOptions = ({formik}) => {
-    const {t} = useTranslation();
-    registerAdvancedOptionsActions(registry, t);
-
     const [activeOption, setActiveOption] = useState('technicalInformation');
+
     const SelectedTabComponents = {
         technicalInformation: TechnicalInformation
     };
