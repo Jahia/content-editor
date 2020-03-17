70f96eda59ca9e408ad6687ec8327b6db6e1d5c4
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-03-17 16:44:17 +0100

    BACKLOG-12810 : refacto detail panel to remove preview button

--- src/javascript/EditPanel/EditPanelContent/PreviewContainer/PreviewContainer.jsx
+++ src/javascript/EditPanel/EditPanelContent/PreviewContainer/PreviewContainer.jsx
@@ -1,113 +1,36 @@
-import {Badge, Paper, Typography} from '@jahia/design-system-kit';
-import {Grid, withStyles} from '@material-ui/core';
+import {Badge, Paper} from '@jahia/design-system-kit';
 import * as PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {Constants} from '~/ContentEditor.constants';
 import {ContentPreviewMemoWrapper} from './Preview';
-import {ToggleButton, ToggleButtonGroup} from '@material-ui/lab';
+import classes from './PreviewContainer.scss';
 
-const styles = theme => ({
-    contentPaper: {
-        display: 'flex',
-        flex: '1 1 auto',
-        flexDirection: 'column',
-        minWidth: 0,
-        paddingTop: theme.spacing.unit * 5,
-        backgroundColor: theme.palette.ui.alpha
-    },
-    badge: {
-        margin: 0
-    },
-    preview: {
-        marginBottom: theme.spacing.unit * 3,
-        marginTop: theme.spacing.unit * -3
-    },
-    toggleButtons: {
-        boxShadow: 'none'
-    },
-    buttonRoot: {
-        margin: 0,
-        backgroundColor: theme.palette.ui.alpha + ' !important',
-        borderBottomStyle: 'solid',
-        borderBottomWidth: 2,
-        borderBottomColor: theme.palette.common.white,
-        '&:hover': {
-            borderBottomColor: theme.palette.border.main
-        }
-    },
-    buttonLabel: {},
-    buttonSelected: {
-        borderBottomColor: theme.palette.primary.main + ' !important'
-    }
-});
-
-const SelectedTabComponents = {
-    preview: ContentPreviewMemoWrapper
-};
-
-const PreviewContainerCmp = ({classes, isDirty, mode}) => {
+export const PreviewContainer = ({isDirty}) => {
     const {t} = useTranslation();
-    const [previewMode, setPreviewMode] = useState(mode === Constants.routes.baseEditRoute ? 'preview' : null);
-
-    const SelectedTabComponent = SelectedTabComponents[previewMode];
-    const PreviewCmp = mode === Constants.routes.baseEditRoute && SelectedTabComponent ?
-        <SelectedTabComponent isDirty={isDirty}/> : null;
-
-    const toggleButtonClasses = {
-        root: classes.buttonRoot,
-        selected: classes.buttonSelected
-    };
 
     return (
-        <Paper className={classes.contentPaper}>
-            <Grid container
-                  direction="row"
-                  justify="space-between"
-                  alignItems="center"
-                  className={classes.preview}
-            >
-                <Grid item>
-                    <ToggleButtonGroup exclusive
-                                       value={previewMode}
-                                       className={classes.toggleButtons}
-                                       onChange={(_, mode) => {
-                                           if (mode) {
-                                               setPreviewMode(mode);
-                                           }
-                                       }}
-                    >
-                        <ToggleButton value="preview" classes={toggleButtonClasses}>
-                            <Typography id="preview-tab">
-                                {t('content-editor:label.contentEditor.preview.toggleButtons.preview')}
-                            </Typography>
-                        </ToggleButton>
-                    </ToggleButtonGroup>
-                </Grid>
-
+        <Paper className={classes.content}>
+            <div className={classes.container}>
                 {isDirty &&
-                <Grid item>
+                <div>
                     <Badge className={classes.badge}
                            badgeContent={t('content-editor:label.contentEditor.preview.updateOnSave')}
                            variant="normal"
                            color="info"
                     />
-                </Grid>}
-            </Grid>
-            {PreviewCmp}
+                </div>}
+            </div>
+            <ContentPreviewMemoWrapper/>
         </Paper>
     );
 };
 
-PreviewContainerCmp.defaultProps = {
+PreviewContainer.defaultProps = {
     isDirty: false
 };
 
-PreviewContainerCmp.propTypes = {
-    classes: PropTypes.object.isRequired,
-    mode: PropTypes.string.isRequired,
+PreviewContainer.propTypes = {
     isDirty: PropTypes.bool
 };
 
-export const PreviewContainer = withStyles(styles)(PreviewContainerCmp);
 PreviewContainer.displayName = 'PreviewContainer';
