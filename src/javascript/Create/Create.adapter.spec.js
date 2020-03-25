34ea7807013683f8dc951a813d2425d5d211c1d2
Kevan <kevan@jahia.com>
2020-03-25 10:17:52 +0100

    BACKLOG-12897: typo in import in test

--- src/javascript/Create/Create.adapter.spec.js
+++ src/javascript/Create/Create.adapter.spec.js
@@ -1,4 +1,4 @@
-import {adaptCreateRequest} from 'Create.adapter';
+import {adaptCreateRequest} from './Create.adapter';
 
 describe('adaptCreate', () => {
     it('should adapt create query with system name data', () => {
