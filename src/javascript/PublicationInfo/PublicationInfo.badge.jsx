9e19b6b9ef0f2afc8c1497f1c2eb2f7fb7bab6b9
Kevan <kevan@jahia.com>
2020-04-03 18:07:19 +0200

    BACKLOG-13045: display 'not published', 'live', 'modified' and 'warning' badges, based on jcontent display rules

--- src/javascript/PublicationInfo/PublicationInfo.badge.jsx
+++ src/javascript/PublicationInfo/PublicationInfo.badge.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import {Chip} from '@jahia/moonstone';
-import {FileContent} from '@jahia/moonstone/dist/icons';
 import {usePublicationInfoContext} from './PublicationInfo.context';
 import {useTranslation} from 'react-i18next';
+import {Constants} from '~/ContentEditor.constants';
+import PublicationStatus from './PublicationInfo.status';
+import {getTooltip} from './PublicationInfo.tooltip';
+import {useContentEditorConfigContext} from '~/ContentEditor.context';
 
 export const PublicationInfoBadge = () => {
-    const {t} = useTranslation();
-    const {publicationInfoPolling} = usePublicationInfoContext();
+    const {t} = useTranslation('content-editor');
+    const publicationInfoContext = usePublicationInfoContext();
+    const {uilang} = useContentEditorConfigContext();
+
+    const statuses = {
+        modified: false,
+        published: false,
+        warning: false
+    };
+
+    // This rules have been extracted from JContent, please maintain this rules to be consistent with JContent
+    if (publicationInfoContext.publicationStatus) {
+        if (publicationInfoContext.publicationStatus === Constants.editPanel.publicationStatus.MODIFIED) {
+            statuses.modified = true;
+            statuses.published = true;
+        } else if (publicationInfoContext.publicationStatus === Constants.editPanel.publicationStatus.NOT_PUBLISHED) {
+            statuses.published = false;
+        } else if (publicationInfoContext.publicationStatus === Constants.editPanel.publicationStatus.PUBLISHED) {
+            statuses.published = true;
+        } else if (publicationInfoContext.publicationStatus === Constants.editPanel.publicationStatus.UNPUBLISHED) {
+            statuses.published = false;
+        } else if (publicationInfoContext.publicationStatus !== Constants.editPanel.publicationStatus.MARKED_FOR_DELETION) {
+            statuses.warning = true;
+        }
+    }
+
+    const supportedUiLang = Constants.supportedLocales.includes(uilang) ? uilang : Constants.defaultLocale;
+    const renderStatus = type => (
+        <PublicationStatus type={type} tooltip={getTooltip(type, publicationInfoContext, supportedUiLang, t)}/>
+    );
     return (
         <>
-            {publicationInfoPolling &&
-            <Chip
-                data-sel-role="publication-info-polling-badge"
-                label={t('content-editor:label.contentEditor.edit.action.publish.badge')}
-                icon={<FileContent/>}
-                color="default"
-            />}
+            {!publicationInfoContext.publicationInfoPolling &&
+            <>
+                {statuses.modified && renderStatus('modified')}
+                {renderStatus(statuses.published ? 'published' : 'notPublished')}
+                {statuses.warning && renderStatus('warning')}
+            </>}
+            {publicationInfoContext.publicationInfoPolling && renderStatus('publishing')}
         </>
     );
 };
