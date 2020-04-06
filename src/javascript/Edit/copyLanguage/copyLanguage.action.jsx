190e1fef1aaf7609b2bd81d72b5352a88ef442db
Taybou <benterki.tayeb@gmail.com>
2020-04-06 10:49:42 +0200

    BACKLOG-13069 update copy language action

--- src/javascript/Edit/copyLanguage/copyLanguage.action.jsx
+++ src/javascript/Edit/copyLanguage/copyLanguage.action.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import {composeActions, componentRendererAction} from '@jahia/react-material';
-import {editRestrictedAction} from '~/actions/editRestricted.action';
+import React, {useContext} from 'react';
+import PropTypes from 'prop-types';
 import {CopyLanguageDialog} from '~/EditPanel/CopyLanguageDialog';
 import {getFullLanguageName} from './copyLanguage.utils';
+import {ComponentRendererContext} from '@jahia/ui-extender';
 
-export default composeActions(
-    editRestrictedAction,
-    componentRendererAction,
-    {
-        init: context => {
-            context.enabled = context.siteInfo.languages.length > 1;
-        },
-        onClick: context => {
-            if (context.enabled) {
-                const handler = context.renderComponent(
-                    <CopyLanguageDialog
-                        isOpen
-                        uuid={context.nodeData.uuid}
-                        language={getFullLanguageName(context.siteInfo.languages, context.language)}
-                        availableLanguages={context.siteInfo.languages}
-                        formik={context.formik}
-                        onCloseDialog={() => handler.setProps({isOpen: false})}
-                    />);
+export const CopyLanguageActionComponent = ({context, render: Render}) => {
+    const {render, destroy} = useContext(ComponentRendererContext);
+
+    return (
+        <Render context={{
+            ...context,
+            enabled: context.siteInfo.languages.length > 1,
+            onClick: () => {
+                render('CopyLanguageDialog', CopyLanguageDialog, {
+                    isOpen: true,
+                    uuid: context.nodeData.uuid,
+                    language: getFullLanguageName(context.siteInfo.languages, context.language),
+                    availableLanguages: context.siteInfo.languages,
+                    formik: context.formik,
+                    onCloseDialog: () => destroy('CopyLanguageDialog')
+                });
             }
-        }
-    }
-);
+        }}/>
+    );
+};
+
+CopyLanguageActionComponent.propTypes = {
+    context: PropTypes.object.isRequired,
+    render: PropTypes.func.isRequired
+};
