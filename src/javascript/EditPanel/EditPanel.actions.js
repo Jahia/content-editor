1e827ee7a1121201a0fa44463b5e22aa02a7969b
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-04-15 10:38:12 +0200

    DMF-3817 : Allows to add tabs action

--- src/javascript/EditPanel/EditPanel.actions.js
+++ src/javascript/EditPanel/EditPanel.actions.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TabBarAction from './tabbar/tabbar.action';
 import {Edit, Settings} from '@material-ui/icons';
 import {Constants} from '~/ContentEditor.constants';
+import EditPanelContent from '~/EditPanel/EditPanelContent/EditPanelContent';
+import AdvancedOptions from '~/EditPanel/AdvancedOptions/AdvancedOptions';
 
 export const registerEditPanelActions = actionsRegistry => {
     // Tab bar actions
@@ -11,15 +13,17 @@ export const registerEditPanelActions = actionsRegistry => {
         targets: ['editHeaderTabsActions:1'],
         value: 'edit',
         dataSelRole: 'tab-edit',
+        displayableComponent: EditPanelContent,
         isDisplayable: () => true
     });
 
     actionsRegistry.add('action', 'ceAdvancedTab', TabBarAction, {
         buttonLabel: 'content-editor:label.contentEditor.edit.tab.advanced',
         buttonIcon: <Settings/>,
-        targets: ['editHeaderTabsActions:1'],
+        targets: ['editHeaderTabsActions:2'],
         value: 'advanced',
         dataSelRole: 'tab-advanced-options',
+        displayableComponent: AdvancedOptions,
         isDisplayable: context => context.mode === Constants.routes.baseEditRoute
     });
 };
