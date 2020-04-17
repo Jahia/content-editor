2977dc660b73a354caed0d1c2cdb7b62d316adb7
FBerthelot <florent.berthelot35@gmail.com>
2020-04-17 15:26:37 +0200

    BACKLOG-13232: delay the display of preview in content editor and add some vanilla improvment

---
+++ src/javascript/EditPanel/EditPanelContent/Preview/Preview.container.jsx
@@ -0,0 +1,70 @@
+import {Badge, Paper} from '@jahia/design-system-kit';
+import * as PropTypes from 'prop-types';
+import React, {useEffect, useState, useCallback} from 'react';
+import {useTranslation} from 'react-i18next';
+import classes from './Preview.container.scss';
+import {ProgressOverlay} from '@jahia/react-material';
+import {useContentEditorContext} from '~/ContentEditor.context';
+import {PreviewFetcher} from './Preview.fetcher';
+
+export const PreviewContainer = ({isDirty}) => {
+    const {t} = useTranslation();
+    const editorContext = useContentEditorContext();
+    const [contentNotFound, setContentNotFound] = useState(false);
+    const handleContentNotFound = useCallback(() => setContentNotFound(true), []);
+    const [shouldDisplayPreview, setShouldDisplayIframe] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setShouldDisplayIframe(true);
+        }, 1500);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, []);
+
+    return (
+        <Paper className={classes.content}>
+            <div className={classes.container}>
+                {isDirty &&
+                    <div>
+                        <Badge className={classes.badge}
+                               badgeContent={t('content-editor:label.contentEditor.preview.updateOnSave')}
+                               variant="normal"
+                               color="info"
+                        />
+                    </div>}
+                {contentNotFound &&
+                    <div>
+                        <Badge
+                            badgeContent={t('content-editor:label.contentEditor.preview.contentNotFound')}
+                            variant="normal"
+                            color="warning"
+                        />
+                    </div>}
+                {editorContext.nodeData.isFolder &&
+                    <div>
+                        <Badge
+                            badgeContent={t('content-editor:label.contentEditor.preview.noPreview')}
+                            variant="normal"
+                            color="warning"
+                        />
+                    </div>}
+            </div>
+            {!editorContext.nodeData.isFolder &&
+                <>
+                    {shouldDisplayPreview ?
+                        <PreviewFetcher onContentNotFound={handleContentNotFound}/> :
+                        <ProgressOverlay/>}
+                </>}
+        </Paper>
+    );
+};
+
+PreviewContainer.defaultProps = {
+    isDirty: false
+};
+
+PreviewContainer.propTypes = {
+    isDirty: PropTypes.bool
+};
