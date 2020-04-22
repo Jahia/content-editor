048b8c1999c32c3f97e5dce9486874f5ae81e166
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-04-22 16:27:54 +0200

    BACKLOG-13082 : remove / character in the system name and replace *

--- src/javascript/utils/helper.spec.js
+++ src/javascript/utils/helper.spec.js
@@ -13,14 +13,14 @@ describe('truncate', () => {
 describe('System name encoding/decoding', () => {
     it('Should encode system name', () => {
         expect(encodeSystemName('test-system-name-classic')).toBe('test-system-name-classic');
-        expect(encodeSystemName('invalid-chars-/:[]|*%')).toBe('invalid-chars-%2F%3A%5B%5D%7C%9A%25');
+        expect(encodeSystemName('invalid-chars-/:[]|*%')).toBe('invalid-chars-%3A%5B%5D%7C%2A%25');
         // %9A is used to encode manually '*' it should be handled correctly
         expect(encodeSystemName('enforce-test-%9A')).toBe('enforce-test-%259A');
     });
 
     it('should decode system name', () => {
         expect(decodeSystemName('test-system-name-classic')).toBe('test-system-name-classic');
-        expect(decodeSystemName('invalid-chars-%2F%3A%5B%5D%7C%9A%25')).toBe('invalid-chars-/:[]|*%');
+        expect(decodeSystemName('invalid-chars-%3A%5B%5D%7C%2A%25')).toBe('invalid-chars-:[]|*%');
         // %9A is used to encode manually '*' it should be handled correctly
         expect(decodeSystemName('enforce-test-%259A')).toBe('enforce-test-%9A');
     });
