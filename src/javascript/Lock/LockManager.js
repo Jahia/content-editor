f51d7c7896cccd17f3355d77c2d33bc5a769560b
kevan Jahanshahi <kevan@jahia.com>
2020-04-22 16:51:43 +0200

    BACKLOG-13327: fix lock behavior and simplify code (#658)

--- src/javascript/Lock/LockManager.js
+++ src/javascript/Lock/LockManager.js
@@ -1,25 +1,34 @@
 import PropTypes from 'prop-types';
 import {useEffect} from 'react';
-import {useSubscription, useApolloClient} from '@apollo/react-hooks';
-import {useEditorIdContext} from '../ContentEditorId.context';
+import {useApolloClient} from '@apollo/react-hooks';
 import {SubscribeToEditorLock} from './lock.gql-subscription';
 import {UnlockEditorMutation} from './lock.gql-mutation';
 
 export const LockManager = ({path}) => {
-    const {editorId} = useEditorIdContext();
     const client = useApolloClient();
 
-    // Lock and subscribe to ping the server we are editing
-    useSubscription(SubscribeToEditorLock, {
-        variables: {
-            nodePath: path,
-            editorID: editorId
-        },
-        shouldResubscribe: true
-    });
-
     useEffect(() => {
-        const unlock = () => {
+        // Generate random id to hold the lock server side for current editor
+        const editorId = '_' + Math.random().toString(36).substr(2, 9);
+
+        // Lock and subscribe to ping the server we are editing,
+        // The subscription is used in case the browser is closed or crashed, to unlock automatically the node.
+        // The server is sending heartbeat messages through this subscription, in case the subscription is closed client side (browser closed, crashed, etc)
+        // Then the node will be automatically unlocked by the server after a few seconds.
+        const subscriptionObserver = client.subscribe({
+            query: SubscribeToEditorLock,
+            variables: {
+                nodePath: path,
+                editorID: editorId
+            },
+            shouldResubscribe: true
+        });
+
+        const subscription = subscriptionObserver.subscribe(() => {/* Nothing to do on heartbeat messages */});
+
+        // Manually unlock when this component is unload.
+        // Even if the subscription is still running it will be killed automatically after the server timed out on heartbeat message sending
+        return () => {
             client
                 .mutate({
                     mutation: UnlockEditorMutation,
@@ -27,13 +36,13 @@ export const LockManager = ({path}) => {
                         editorID: editorId
                     }
                 })
+                .then(() => {
+                    // Clear subscription client side. (this will not stop the server side subscription, the server side subscription will end by timeout)
+                    subscription.unsubscribe();
+                })
                 .catch(err => console.error(err));
         };
-
-        return () => {
-            unlock();
-        };
-    }, [client, editorId]);
+    }, [client, path]);
 
     return '';
 };
