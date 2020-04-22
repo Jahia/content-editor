c5dd17c9fd85c6860a7730678ac64e252d8de071
MT BENTERKI <benterki.tayeb@gmail.com>
2020-04-22 15:24:00 +0200

    QA-12571 show placeholder in case of error (#655)

--- src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Picker/PickerDialog/Views/Thumbnail/Thumbnail.jsx
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Picker/PickerDialog/Views/Thumbnail/Thumbnail.jsx
@@ -39,44 +39,46 @@ export const Thumbnail = ({
             'content-media-manager:label.contentManager.error.queryingContent',
             {details: error.message ? error.message : ''}
         );
-        return <>{message}</>;
+        console.warn(message);
     }
 
     if (loading || !nodes) {
         return <ProgressOverlay/>;
     }
 
-    const images = nodes.map(rawImg => {
-        return {
-            uuid: rawImg.uuid,
-            url: `${
-                window.contextJsParameters.contextPath
-            }/files/default${encodeJCRPath(rawImg.path)}?lastModified=${rawImg.lastModified.value}&t=thumbnail2`,
-            path: rawImg.path,
-            name: rawImg.displayName,
-            type: rawImg.metadata.nodes[0].mimeType.value.replace('image/', ''),
-            width: rawImg.width ? `${rawImg.width.value}` : null,
-            height: rawImg.height ? `${rawImg.height.value}` : null
-        };
-    });
+    const images = error ?
+        [] :
+        nodes.map(rawImg => {
+            return {
+                uuid: rawImg.uuid,
+                url: `${
+                    window.contextJsParameters.contextPath
+                }/files/default${encodeJCRPath(rawImg.path)}?lastModified=${rawImg.lastModified.value}&t=thumbnail2`,
+                path: rawImg.path,
+                name: rawImg.displayName,
+                type: rawImg.metadata.nodes[0].mimeType.value.replace('image/', ''),
+                width: rawImg.width ? `${rawImg.width.value}` : null,
+                height: rawImg.height ? `${rawImg.height.value}` : null
+            };
+        });
 
     return (
         <>
             <CountDisplayer totalCount={totalCount}/>
             <ImageList
-            labelEmpty={
-                searchTerms ?
-                    t('content-editor:label.contentEditor.edit.fields.contentPicker.noSearchResults') :
-                    t('content-editor:label.contentEditor.edit.fields.contentPicker.noContent')
-            }
-            images={images}
-            hasMore={hasMore}
-            loadMore={loadMore}
-            error={error}
-            initialSelection={initialSelection}
-            onImageSelection={setSelectedItem}
-            onImageDoubleClick={onThumbnailDoubleClick}
-        />
+                labelEmpty={
+                    searchTerms ?
+                        t('content-editor:label.contentEditor.edit.fields.contentPicker.noSearchResults') :
+                        t('content-editor:label.contentEditor.edit.fields.contentPicker.noContent')
+                }
+                images={images}
+                hasMore={hasMore}
+                loadMore={loadMore}
+                error={error}
+                initialSelection={initialSelection}
+                onImageSelection={setSelectedItem}
+                onImageDoubleClick={onThumbnailDoubleClick}
+            />
         </>
     );
 };
