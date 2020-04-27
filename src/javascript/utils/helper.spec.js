1d8b4112c8611fdd6e09e1f2652c14367c8e3756
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-04-27 15:48:45 +0200

    BACKLOG-13082 : replace / and : character in system name

--- src/javascript/utils/helper.spec.js
+++ src/javascript/utils/helper.spec.js
@@ -13,7 +13,7 @@ describe('truncate', () => {
 describe('System name encoding/decoding', () => {
     it('Should encode system name', () => {
         expect(encodeSystemName('test-system-name-classic')).toBe('test-system-name-classic');
-        expect(encodeSystemName('invalid-chars-/:[]|*%')).toBe('invalid-chars-%3A%5B%5D%7C%2A%25');
+        expect(encodeSystemName('invalid-chars-/:[]|*%')).toBe('invalid-chars-%5B%5D%7C%2A%25');
         // %9A is used to encode manually '*' it should be handled correctly
         expect(encodeSystemName('enforce-test-%9A')).toBe('enforce-test-%259A');
     });
