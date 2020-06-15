241d37728827b57a2e6ad260926f3fd2e9e80be5
Kevan <kevan@jahia.com>
2020-06-15 18:07:20 +0200

    BACKLOG-13868: adapt Text selector type to global onChange logic

--- src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Text/Text.jsx
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Text/Text.jsx
@@ -2,39 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Input} from '@jahia/design-system-kit';
 import {FieldPropTypes} from '~/FormDefinitions/FormData.proptypes';
-import {FastField} from 'formik';
 
-export const TextCmp = ({field, value, id, editorContext, onChange}) => {
+export const TextCmp = ({field, value, id, editorContext, onChange, onInit}) => {
     const fieldType = field.requiredType;
     const isNumber = fieldType === 'DOUBLE' || fieldType === 'LONG' || fieldType === 'DECIMAL';
     const decimalSeparator = editorContext.uilang === 'en' ? '.' : ',';
     const controlledValue = value === undefined ? '' : (isNumber ? value?.replace('.', decimalSeparator) : value);
-    return (
-        <FastField render={({form: {handleChange, setFieldTouched}}) => {
-            const onFieldChange = evt => {
-                handleChange(evt);
-                setFieldTouched(field.name, field.multiple ? [true] : true);
-                onChange(controlledValue, evt?.target.value);
-            };
 
-            return (
-                <Input
-                    fullWidth
-                    id={id}
-                    name={id}
-                    inputProps={{
-                        'aria-labelledby': `${field.name}-label`,
-                        'aria-required': field.mandatory
-                    }}
-                    value={controlledValue}
-                    readOnly={field.readOnly}
-                    type={isNumber ? 'number' : 'text'}
-                    decimalScale={fieldType === 'LONG' ? 0 : undefined}
-                    decimalSeparator={decimalSeparator}
-                    onChange={onFieldChange}
-                />
-            );
-        }}
+    onInit(controlledValue);
+
+    return (
+        <Input
+            fullWidth
+            id={id}
+            name={id}
+            inputProps={{
+                'aria-labelledby': `${field.name}-label`,
+                'aria-required': field.mandatory
+            }}
+            value={controlledValue}
+            readOnly={field.readOnly}
+            type={isNumber ? 'number' : 'text'}
+            decimalScale={fieldType === 'LONG' ? 0 : undefined}
+            decimalSeparator={decimalSeparator}
+            onChange={evt => onChange(evt?.target?.value)}
         />
     );
 };
@@ -44,7 +35,8 @@ TextCmp.propTypes = {
     value: PropTypes.string,
     editorContext: PropTypes.object.isRequired,
     field: FieldPropTypes.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    onInit: PropTypes.func.isRequired
 };
 
 const Text = TextCmp;
