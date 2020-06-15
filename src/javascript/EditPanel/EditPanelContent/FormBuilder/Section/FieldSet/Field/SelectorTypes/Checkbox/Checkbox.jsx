3c0442814c36361cd685d734531d1cbf1954f6d2
David Griffon <dgriffon@jahia.com>
2020-06-15 17:58:24 +0200

    adapt checkbox selector

--- src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Checkbox/Checkbox.jsx
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SelectorTypes/Checkbox/Checkbox.jsx
@@ -2,29 +2,21 @@ import React from 'react';
 import * as PropTypes from 'prop-types';
 import {Toggle} from '@jahia/design-system-kit';
 import {FieldPropTypes} from '~/FormDefinitions/FormData.proptypes';
-import {FastField} from 'formik';
 
 const Checkbox = ({field, value, id, onChange}) => {
-    return (
-        <FastField name={field.name}
-                   render={({form: {setFieldValue, setFieldTouched}, field: formikField}) => {
-            const handleChange = (event, checked) => {
-                setFieldValue(id, checked);
-                setFieldTouched(id, field.multiple ? [true] : true);
-                onChange(formikField.value, checked);
-            };
+    const handleChange = (event, checked) => {
+        onChange(checked);
+    };
 
-            return (
-                <Toggle id={id}
-                        inputProps={{
-                            'aria-labelledby': `${field.name}-label`
-                        }}
-                        checked={value === true}
-                        readOnly={field.readOnly}
-                        onChange={handleChange}
-                />
-);
-        }}/>
+    return (
+        <Toggle id={id}
+                inputProps={{
+                    'aria-labelledby': `${field.name}-label`
+                }}
+                checked={value === true}
+                readOnly={field.readOnly}
+                onChange={handleChange}
+        />
     );
 };
 
