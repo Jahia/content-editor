009d2cb003d5751d140723af6395b40110bb6fac
Kevan <kevan@jahia.com>
2020-06-17 16:22:26 +0200

    BACKLOG-13909: Add react unit test on MultipleField and SingleField onChange logic

--- src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/MultipleField.spec.jsx
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/MultipleField.spec.jsx
@@ -4,10 +4,19 @@ import {shallowWithTheme} from '@jahia/test-framework';
 import {dsGenericTheme} from '@jahia/design-system-kit';
 import Text from './SelectorTypes/Text/Text';
 import {MultipleFieldCmp} from './MultipleField';
+import {TextAreaField} from './SelectorTypes/TextArea/TextArea';
+
+jest.mock('react', () => {
+    return {
+        ...jest.requireActual('react'),
+        useEffect: cb => cb()
+    };
+});
 
 describe('Multiple component', () => {
     let defaultProps;
     let defaultPropsFieldArray;
+    let defaultPropsFastField;
 
     beforeEach(() => {
         defaultProps = {
@@ -37,16 +46,23 @@ describe('Multiple component', () => {
             },
             classes: {},
             t: jest.fn(),
-            remove: jest.fn()
+            remove: jest.fn(),
+            onChange: jest.fn()
         };
 
         defaultPropsFieldArray = {
             remove: jest.fn(),
             push: jest.fn()
         };
+
+        defaultPropsFastField = {
+            form: {
+                setFieldValue: jest.fn(),
+                setFieldTouched: jest.fn()
+            }
+        };
     });
 
-    /* TODO fix
     it('should contains multiple fields', () => {
         defaultProps.inputContext.fieldComponent = props => <Text {...props}/>;
         const cmp = shallowWithTheme(
@@ -55,20 +71,57 @@ describe('Multiple component', () => {
             dsGenericTheme
         );
 
-        const debug = cmp.dive().dive().dive().debug();
-        expect(debug).toContain('value="Dummy1" id="text[0]"');
-        expect(debug).toContain('value="Dummy2" id="text[1]"');
-    }); */
+        expect(cmp.dive().dive().dive().find('FormikConnect(FastFieldInner)').length).toBe(2);
+    });
 
-    /* TODO fix
     it('should call onClick when click on remove button', () => {
-        generateFieldArrayCmp().find('DsIconButton').at(1).simulate('click');
+        const arraycmp = generateFieldArrayCmp();
+        generateFieldCmp(arraycmp, 0).dive().dive().dive().debug();
+        generateFieldCmp(arraycmp, 1).dive().dive().dive().debug();
+        generateFieldCmp(arraycmp, 2).dive().dive().dive().debug();
+
+        arraycmp.find('DsIconButton').at(1).simulate('click');
         expect(defaultPropsFieldArray.remove).toHaveBeenCalled();
-    }); */
+    });
 
     it('should call onClick when click on add button', () => {
         generateFieldArrayCmp().find('DsButton').simulate('click');
         expect(defaultPropsFieldArray.push).toHaveBeenCalled();
+    });
+
+    it('should call onChange', () => {
+        const arraycmp = generateFieldArrayCmp();
+        generateFieldCmp(arraycmp, 0).dive().dive();
+        const field2 = generateFieldCmp(arraycmp, 1).dive().dive();
+        generateFieldCmp(arraycmp, 2).dive().dive();
+
+        // Init should call onChange with initial values
+        expect(defaultProps.onChange.mock.calls.length).toBe(1);
+        expect(defaultProps.onChange).toHaveBeenCalledWith(undefined, ['Dummy1', 'Dummy2', 'Dummy3']);
+
+        // Change field2
+        defaultProps.onChange.mockReset();
+        field2.simulate('change', {
+            target: {
+                value: 'Updated2'
+            }
+        });
+        expect(defaultPropsFastField.form.setFieldValue).toHaveBeenCalledWith('text[1]', 'Updated2', true);
+        expect(defaultPropsFastField.form.setFieldTouched).toHaveBeenCalledWith('text', [true]);
+        expect(defaultProps.onChange.mock.calls.length).toBe(1);
+        expect(defaultProps.onChange).toHaveBeenCalledWith(['Dummy1', 'Dummy2', 'Dummy3'], ['Dummy1', 'Updated2', 'Dummy3']);
+
+        // Remove Dummy1
+        defaultProps.onChange.mockReset();
+        arraycmp.find('DsIconButton').at(0).simulate('click');
+        expect(defaultProps.onChange.mock.calls.length).toBe(1);
+        expect(defaultProps.onChange).toHaveBeenCalledWith(['Dummy1', 'Updated2', 'Dummy3'], ['Updated2', 'Dummy3']);
+
+        // Add value should not trigger onChange
+        defaultProps.onChange.mockReset();
+        arraycmp.find('DsButton').simulate('click');
+        expect(defaultProps.onChange).not.toHaveBeenCalled();
+        console.log('tolo');
     });
 
     it('should display remove button when field is not readOnly', () => {
@@ -102,7 +155,7 @@ describe('Multiple component', () => {
             }
         };
 
-        defaultProps.inputContext.fieldComponent = props => <Text {...props}/>;
+        defaultProps.inputContext.fieldComponent = props => <TextAreaField {...props}/>;
         let cmp = shallowWithTheme(
             <MultipleFieldCmp {...defaultProps}/>,
             {},
@@ -112,6 +165,15 @@ describe('Multiple component', () => {
         const FieldArrayRender = cmp.dive().dive().props().render;
         return shallowWithTheme(
             <FieldArrayRender {...defaultPropsFieldArray}/>,
+            {},
+            dsGenericTheme
+        );
+    };
+
+    let generateFieldCmp = (arrayCmp, index) => {
+        const FieldRender = arrayCmp.find('FormikConnect(FastFieldInner)').at(index).props().render;
+        return shallowWithTheme(
+            <FieldRender {...defaultPropsFastField}/>,
             {},
             dsGenericTheme
         );
