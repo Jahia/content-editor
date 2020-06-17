88a59278abee0560875fa52e51aca42ebc106146
kevan Jahanshahi <kevan@jahia.com>
2020-06-17 17:24:01 +0200

    BACKLOG-13909: Add react unit test on MultipleField and SingleField onChange logic (#717)

--- src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SingleField.spec.jsx
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Section/FieldSet/Field/SingleField.spec.jsx
@@ -1,13 +1,20 @@
-/* TODO fix
 import React from 'react';
 import {shallowWithTheme} from '@jahia/test-framework';
 
 import {dsGenericTheme} from '@jahia/design-system-kit';
-import Text from './SelectorTypes/Text/Text';
 import {SingleFieldCmp} from './SingleField';
+import {TextAreaField} from './SelectorTypes/TextArea/TextArea';
+
+jest.mock('react', () => {
+    return {
+        ...jest.requireActual('react'),
+        useEffect: cb => cb()
+    };
+});
 
 describe('Field component', () => {
     let defaultProps;
+    let defaultPropsFastField;
 
     beforeEach(() => {
         defaultProps = {
@@ -34,24 +41,58 @@ describe('Field component', () => {
             inputContext: {
                 fieldComponent: () => <></>,
                 editorContext: {}
+            },
+            onChange: jest.fn()
+        };
+
+        defaultPropsFastField = {
+            form: {
+                setFieldValue: jest.fn(),
+                setFieldTouched: jest.fn()
             }
         };
     });
 
     it('the field should have a defined id attribute', () => {
-        defaultProps.inputContext.fieldComponent = props => <Text {...props}/>;
+        defaultProps.inputContext.fieldComponent = props => <TextAreaField {...props}/>;
+        const cmp = buildFieldCmp();
+
+        expect(cmp.debug()).toContain('id="text"');
+    });
+
+    it('Should call onChange', () => {
+        defaultProps.inputContext.fieldComponent = props => <TextAreaField {...props}/>;
+        const cmp = buildFieldCmp().dive().dive();
+
+        // Init should call onChange with initial values
+        expect(defaultProps.onChange.mock.calls.length).toBe(1);
+        expect(defaultProps.onChange).toHaveBeenCalledWith(undefined, 'Dummy');
+
+        // Update field
+        defaultProps.onChange.mockReset();
+        cmp.simulate('change', {
+            target: {
+                value: 'Updated'
+            }
+        });
+        expect(defaultPropsFastField.form.setFieldValue).toHaveBeenCalledWith('text', 'Updated', true);
+        expect(defaultPropsFastField.form.setFieldTouched).toHaveBeenCalledWith('text', true);
+        expect(defaultProps.onChange.mock.calls.length).toBe(1);
+        expect(defaultProps.onChange).toHaveBeenCalledWith('Dummy', 'Updated');
+    });
+
+    let buildFieldCmp = () => {
         const cmp = shallowWithTheme(
             <SingleFieldCmp {...defaultProps}/>,
             {},
             dsGenericTheme
         );
 
-        expect(cmp.debug()).toContain('id="text"');
-    });
-}); */
-
-describe('Field component', () => {
-    it('the field should have a defined id attribute', () => {
-        console.log('TODO');
-    });
+        const FastFieldRender = cmp.dive().dive().props().render;
+        return shallowWithTheme(
+            <FastFieldRender {...defaultPropsFastField}/>,
+            {},
+            dsGenericTheme
+        );
+    };
 });
