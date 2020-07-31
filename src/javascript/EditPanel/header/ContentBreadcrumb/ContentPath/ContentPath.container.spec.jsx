6356c08e84aab9b66540fb7e68e0fe0a704d7f97
MT BENTERKI <benterki.tayeb@gmail.com>
2020-07-31 13:02:09 +0200

    BACKLOG-14189 add unit tests for breadcrumb component (#785)

---
+++ src/javascript/EditPanel/header/ContentBreadcrumb/ContentPath/ContentPath.container.spec.jsx
@@ -0,0 +1,196 @@
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useQuery} from '@apollo/react-hooks';
+import {shallow} from '@jahia/test-framework';
+
+import {push} from 'connected-react-router';
+import {cmGoto} from '~/JContent.redux-actions';
+import {useContentEditorConfigContext} from '~/ContentEditor.context';
+
+import {GetContentPath} from './ContentPath.gql-queries';
+import ContentPathContainer from './ContentPath.container';
+
+jest.mock('connected-react-router', () => ({
+    push: jest.fn()
+}));
+
+jest.mock('~/JContent.redux-actions', () => ({
+    cmGoto: jest.fn()
+}));
+
+jest.mock('~/ContentEditor.context', () => ({
+    useContentEditorConfigContext: jest.fn()
+}));
+
+jest.mock('react-redux', () => ({
+    useDispatch: jest.fn(),
+    useSelector: jest.fn()
+}));
+
+jest.mock('@apollo/react-hooks', () => ({
+    useQuery: jest.fn().mockReturnValue({})
+}));
+
+describe('ContentPathContainer', () => {
+    let defaultProps;
+
+    let dispatch = jest.fn();
+    let envProps = {
+        back: jest.fn(),
+        shouldRedirectBeadCrumb: () => false
+    };
+
+    beforeEach(() => {
+        defaultProps = {
+            formik: {
+                dirty: false
+            },
+            uuid: '123',
+            operator: 'op'
+        };
+
+        useSelector.mockImplementation(callback => callback({
+            language: 'fr'
+        }));
+
+        useContentEditorConfigContext.mockImplementation(() => ({
+            envProps: envProps,
+            site: 'mySiteXD'
+        }));
+
+        useDispatch.mockImplementation(() => dispatch);
+    });
+
+    afterEach(() => {
+        dispatch = jest.fn();
+        envProps = {
+            back: jest.fn(),
+            shouldRedirectBeadCrumb: () => false
+        };
+
+        useQuery.mockClear();
+        useDispatch.mockClear();
+        useSelector.mockClear();
+    });
+
+    it('uses expected query parameters', () => {
+        shallow(<ContentPathContainer path="/x/y/z" {...defaultProps}/>);
+
+        expect(useQuery).toHaveBeenCalledWith(GetContentPath, {
+            variables: {
+                path: '/x/y/z',
+                language: 'fr'
+            }
+        });
+    });
+
+    it('renders correctly', () => {
+        const ancestors = [
+            {uuid: 'x', path: '/x'},
+            {uuid: 'y', path: '/x/y'}
+        ];
+
+        useQuery.mockImplementation(() => ({
+            data: {
+                jcr: {
+                    node: {
+                        ancestors: ancestors
+                    }
+                }
+            }
+        }));
+
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        expect(wrapper.find('ContentPath').prop('items')).toEqual(ancestors);
+        expect(wrapper.find('ContentPath').prop('onItemClick')).toBeDefined();
+    });
+
+    it('Something is not right', () => {
+        console.log = jest.fn();
+
+        useQuery.mockImplementation(() => ({
+            error: {
+                message: 'The error is here!',
+                code: '25'
+            }
+        }));
+
+        shallow(<ContentPathContainer {...defaultProps}/>);
+        expect(console.log).toHaveBeenCalledWith({
+            message: 'The error is here!',
+            code: '25'
+        });
+    });
+
+    it('handle redirects on item click', () => {
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        wrapper.find('ContentPath').simulate('itemClick', '/x/y/z');
+
+        expect(dispatch).toHaveBeenCalled();
+        expect(cmGoto).toHaveBeenCalledWith({mode: 'pages', path: '/x/y/z'});
+        expect(envProps.back).not.toHaveBeenCalled();
+        expect(wrapper.find('EditPanelDialogConfirmation').props.open).toBeFalsy();
+    });
+
+    it('handle redirects on item click with dirty form', () => {
+        defaultProps.formik.dirty = true;
+
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        wrapper.find('ContentPath').simulate('itemClick', '/x/y/z');
+
+        expect(dispatch).not.toHaveBeenCalled();
+        expect(envProps.back).not.toHaveBeenCalled();
+
+        expect(wrapper.find('EditPanelDialogConfirmation').props().open).toBeTruthy();
+
+        wrapper.find('EditPanelDialogConfirmation').simulate('closeDialog');
+        expect(wrapper.find('EditPanelDialogConfirmation').props().open).toBeFalsy();
+
+        wrapper.find('ContentPath').simulate('itemClick', '/x/y/z');
+        expect(wrapper.find('EditPanelDialogConfirmation').props().open).toBeTruthy();
+        wrapper.find('EditPanelDialogConfirmation').props().actionCallback();
+        expect(envProps.back).toHaveBeenCalledWith('123', 'op');
+    });
+
+    it('handle redirects on item click when path start with files', () => {
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        wrapper.find('ContentPath').simulate('itemClick', '/sites/mySiteXD/files/chocolate');
+
+        expect(dispatch).toHaveBeenCalled();
+        expect(cmGoto).toHaveBeenCalledWith({mode: 'media', path: '/sites/mySiteXD/files/chocolate'});
+        expect(envProps.back).not.toHaveBeenCalled();
+        expect(wrapper.find('EditPanelDialogConfirmation').props.open).toBeFalsy();
+    });
+
+    it('handle redirects on item click when path start with contents', () => {
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        wrapper.find('ContentPath').simulate('itemClick', '/sites/mySiteXD/contents/fruits');
+
+        expect(dispatch).toHaveBeenCalled();
+        expect(cmGoto).toHaveBeenCalledWith({mode: 'content-folders', path: '/sites/mySiteXD/contents/fruits'});
+        expect(envProps.back).not.toHaveBeenCalled();
+        expect(wrapper.find('EditPanelDialogConfirmation').props.open).toBeFalsy();
+    });
+
+    it('handle redirects on item click with option to go back', () => {
+        envProps.shouldRedirectBeadCrumb = () => true;
+
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        wrapper.find('ContentPath').simulate('itemClick', '/sites/mySiteXD/lord/rings');
+
+        expect(dispatch).toHaveBeenCalled();
+        expect(cmGoto).toHaveBeenCalledWith({mode: 'pages', path: '/sites/mySiteXD/lord/rings'});
+        expect(envProps.back).toHaveBeenCalled();
+        expect(wrapper.find('EditPanelDialogConfirmation').props.open).toBeFalsy();
+    });
+
+    it('handle redirects on item click when path start with categories', () => {
+        const wrapper = shallow(<ContentPathContainer {...defaultProps}/>);
+        wrapper.find('ContentPath').simulate('itemClick', '/sites/systemsite/categories/tennis');
+
+        expect(dispatch).toHaveBeenCalled();
+        expect(push).toHaveBeenCalledWith('/category-manager');
+        expect(envProps.back).not.toHaveBeenCalled();
+        expect(wrapper.find('EditPanelDialogConfirmation').props.open).toBeFalsy();
+    });
+});
