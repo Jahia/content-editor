a1e2c13b499eb14f60927f8d8b7820e45fd9128b
kevan Jahanshahi <kevan@jahia.com>
2020-08-11 11:09:15 +0200

    BACKLOG-14328: Add sync button for system name to sync with jcr:title (#812)
    * BACKLOG-14328: Add sync button for system name to sync with jcr:title
    
    * BACKLOG-14328: improve CSS

--- src/javascript/SelectorTypes/SystemName/SystemName.jsx
+++ src/javascript/SelectorTypes/SystemName/SystemName.jsx
@@ -3,23 +3,43 @@ import React from 'react';
 import {FieldPropTypes} from '~/FormDefinitions/FormData.proptypes';
 import Text from '~/SelectorTypes/Text';
 import {Constants} from '~/ContentEditor.constants';
+import {Button, Copy} from '@jahia/moonstone';
+import {useTranslation} from 'react-i18next';
+import classes from './SystemName.scss';
 
-export const SystemNameCmp = ({field, value, id, editorContext, onChange}) => {
-    // TODO: BACKLOG-14328 sync button
+export const SystemNameCmp = ({field, value, values, id, editorContext, onChange}) => {
+    const {t} = useTranslation();
+
     return (
-        <Text
-            field={{...field, readOnly: field.readOnly || Boolean(editorContext.name && editorContext.mode === Constants.routes.baseCreateRoute)}}
-            value={value}
-            id={id}
-            editorContext={editorContext}
-            onChange={onChange}
-        />
+        <>
+            <Text
+                field={{...field, readOnly: field.readOnly || Boolean(editorContext.name && editorContext.mode === Constants.routes.baseCreateRoute)}}
+                value={value}
+                id={id}
+                editorContext={editorContext}
+                onChange={onChange}
+            />
+
+            {values['jcr:title'] !== undefined && <Button className={classes.syncButton}
+                                                          data-sel-action="syncSystemName"
+                                                          variant="outlined"
+                                                          size="big"
+                                                          color="accent"
+                                                          label={t('content-editor:label.contentEditor.section.fieldSet.system.fields.syncButton')}
+                                                          icon={<Copy/>}
+                                                          isDisabled={field.readOnly || values['jcr:title'] === value}
+                                                          onClick={() => {
+                                                              onChange(values['jcr:title']);
+                                                          }}
+            />}
+        </>
     );
 };
 
 SystemNameCmp.propTypes = {
     id: PropTypes.string.isRequired,
     value: PropTypes.string,
+    values: PropTypes.object.isRequired,
     editorContext: PropTypes.object.isRequired,
     field: FieldPropTypes.isRequired,
     onChange: PropTypes.func.isRequired
