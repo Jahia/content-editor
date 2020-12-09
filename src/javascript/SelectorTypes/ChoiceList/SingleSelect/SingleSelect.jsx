948fe526e6ad3f7513834aeaa825c5aad5025d60
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-12-09 16:07:39 -0500

    BACKLOG-15111: Updates SingleSelect to use Moonstone Dropdown and adjusts tests as necessary

--- src/javascript/SelectorTypes/ChoiceList/SingleSelect/SingleSelect.jsx
+++ src/javascript/SelectorTypes/ChoiceList/SingleSelect/SingleSelect.jsx
@@ -1,26 +1,9 @@
-import {Select, Input} from '@jahia/design-system-kit';
-import {withStyles, MenuItem} from '@material-ui/core';
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'clsx';
 import {FieldPropTypes} from '~/FormDefinitions/FormData.proptypes';
-import {Dropdown, Button, Badge} from '@jahia/moonstone';
+import {Dropdown} from '@jahia/moonstone';
 
-const styles = theme => ({
-    selectField: {
-        width: '100%'
-    },
-    readOnly: {
-        outline: 'none',
-        background: theme.palette.ui.alpha,
-        border: `1px solid ${theme.palette.ui.alpha}!important`,
-        '& div': {
-            cursor: 'default'
-        }
-    }
-});
-
-export const SingleSelectCmp = ({classes, field, value, id, setActionContext, onChange}) => {
+const SingleSelect = ({field, value, id, setActionContext, onChange}) => {
     setActionContext({
         onChange
     });
@@ -38,59 +21,34 @@ export const SingleSelectCmp = ({classes, field, value, id, setActionContext, on
     }
 
     return (
-        <>
-            <Select
-                className={`${classes.selectField} ${readOnly ? classes.readOnly : ''}`}
-                value={value || ''}
-                inputProps={{
-                    name: field.name,
-                    id: id,
-                    'data-sel-content-editor-select-readonly': readOnly
-                }}
-                input={<Input id={id} name={field.name} readOnly={readOnly}/>}
-                onChange={evt => {
-                    if (evt.target.value !== value) {
-                        onChange(evt.target.value);
-                    }
-                }}
-            >
-                {
-                    field.valueConstraints.map(item => {
-                        return (
-                            <MenuItem key={item.value.string} value={item.value.string}>{item.displayValue}</MenuItem>
-                        );
-                    })
+        <Dropdown
+            name={field.name}
+            id={id}
+            data-sel-content-editor-select-readonly={readOnly}
+            isDisabled={readOnly}
+            maxWidth="100%"
+            variant="outlined"
+            size="medium"
+            data={dropdownData}
+            label={label}
+            value={value || ''}
+            onChange={(evt, item) => {
+                if (item.value !== value) {
+                    onChange(item.value);
                 }
-            </Select>
-            <Dropdown
-                name={field.name}
-                isDisabled={readOnly}
-                maxWidth="100%"
-                variant="outlined"
-                size="medium"
-                data={dropdownData}
-                label={label}
-                value={value || ''}
-                onChange={(evt, item) => {
-                    if (item.value !== value) {
-                        onChange(item.value);
-                    }
-                }}
-            />
-        </>
+            }}
+        />
     );
 };
 
-SingleSelectCmp.propTypes = {
+SingleSelect.propTypes = {
     id: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
     field: FieldPropTypes.isRequired,
-    classes: PropTypes.object.isRequired,
     setActionContext: PropTypes.func.isRequired,
     onChange: PropTypes.func.isRequired
 };
 
-const SingleSelect = withStyles(styles)(SingleSelectCmp);
 SingleSelect.displayName = 'SingleSelect';
 
 export default SingleSelect;
--- src/javascript/SelectorTypes/ChoiceList/SingleSelect/SingleSelect.spec.jsx
+++ src/javascript/SelectorTypes/ChoiceList/SingleSelect/SingleSelect.spec.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {shallowWithTheme} from '@jahia/test-framework';
+import {shallow} from '@jahia/test-framework';
 
-import {dsGenericTheme} from '@jahia/design-system-kit';
-import {SingleSelectCmp} from './SingleSelect';
+import SingleSelect from './SingleSelect';
 
 let mockUseEffect = [];
 
@@ -43,12 +42,12 @@ describe('SingleSelect component', () => {
 
     const buildComp = (componentProps, value) => {
         componentProps.value = value;
-        return shallowWithTheme(<SingleSelectCmp {...componentProps}/>, {}, dsGenericTheme);
+        return shallow(<SingleSelect {...componentProps}/>);
     };
 
     it('should bind id correctly', () => {
         const cmp = buildComp(props, 'Yolooo');
-        expect(cmp.props().inputProps.id).toBe(props.id);
+        expect(cmp.props().id).toBe(props.id);
     });
 
     it('should display each option given', () => {
@@ -66,11 +65,9 @@ describe('SingleSelect component', () => {
     it('should select formik value', () => {
         const cmp = buildComp(props);
         const onChangeData = {
-            target: {
-                value: 'Yolooo'
-            }
-        };
-        cmp.simulate('change', onChangeData);
+            value: 'Yolooo'
+        }
+        cmp.simulate('change', null, onChangeData);
 
         expect(onChange).toHaveBeenCalled();
         expect(onChange.mock.calls[0][0]).toStrictEqual('Yolooo');
@@ -87,8 +84,6 @@ describe('SingleSelect component', () => {
     const testReadOnly = function (readOnly) {
         props.field.readOnly = readOnly;
         const cmp = buildComp(props, 'Yolooo');
-        const inputCmp = shallowWithTheme(cmp.props().input, {}, dsGenericTheme);
-
-        expect(inputCmp.props().readOnly).toEqual(readOnly);
+        expect(cmp.props().isDisabled).toEqual(readOnly);
     };
 });
