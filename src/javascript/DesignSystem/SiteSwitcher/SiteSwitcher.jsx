2047c44326a4801f838f7d037f9f81e74a449b25
David Griffon <dgriffon@jahia.com>
2020-12-17 17:23:24 +0100

    BACKLOG-15190 : add Selenium selector attributes (#905)

--- src/javascript/DesignSystem/SiteSwitcher/SiteSwitcher.jsx
+++ src/javascript/DesignSystem/SiteSwitcher/SiteSwitcher.jsx
@@ -7,11 +7,12 @@ const SiteSwitcher = ({id, siteKey, siteNodes, onSelectSite}) => {
     return (
         <Dropdown
             id={id}
+            data-cm-role="site-switcher"
             size="medium"
             value={siteKey}
             className={style.dropdown}
             label={siteNodes.find(siteNode => siteNode.name === siteKey)?.displayName}
-            data={siteNodes.map(siteNode => ({label: siteNode.displayName, value: siteNode.name}))}
+            data={siteNodes.map(siteNode => ({label: siteNode.displayName, value: siteNode.name, attributes: {role: 'menuitem'}}))}
             onChange={onSelectSite}
         />
     );
