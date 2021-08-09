5d715509b06576e0be746a992e7b89afc0738b39
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2021-08-09 15:00:23 -0400

    TECH-210: Update GraphQL dependencies (#980)
    * Refactor graphql context
    
    * TECH-210: Remove graphql annotations dependency version range requirement

--- src/main/java/org/jahia/modules/contenteditor/graphql/api/GqlEditorFormMutations.java
+++ src/main/java/org/jahia/modules/contenteditor/graphql/api/GqlEditorFormMutations.java
@@ -4,14 +4,13 @@ import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import graphql.annotations.annotationTypes.GraphQLNonNull;
+import graphql.kickstart.servlet.context.GraphQLServletContext;
 import graphql.schema.DataFetchingEnvironment;
-import graphql.servlet.GraphQLContext;
 import org.jahia.modules.contenteditor.api.forms.EditorFormException;
 import org.jahia.modules.contenteditor.api.lock.StaticEditorLockService;
 
 import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
 
 /**
  * The root class for the GraphQL form mutations API
@@ -32,13 +31,13 @@ public class GqlEditorFormMutations {
         @GraphQLName("editorID") @GraphQLNonNull @GraphQLDescription("An ID generated client side used to identify the lock") String editorID
     ) throws EditorFormException {
 
-        Optional<HttpServletRequest> httpServletRequest = ((GraphQLContext) environment.getContext()).getRequest();
-        if(!httpServletRequest.isPresent()) {
+        HttpServletRequest httpRequest = ((GraphQLServletContext) environment.getContext()).getHttpServletRequest();
+        if (httpRequest == null) {
             return false;
         }
 
         try {
-            StaticEditorLockService.unlock(httpServletRequest.get(), editorID);
+            StaticEditorLockService.unlock(httpRequest, editorID);
             return true;
         } catch (RepositoryException e) {
             throw new EditorFormException("Unable to unlock content editor", e);
