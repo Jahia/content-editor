e2099b37383f1515047a8e36e43edc781ef5648a
Thomas Draier <tdraier@jahia.com>
2022-02-08 10:32:18 +0100

    BACKLOG-16797: upgrade libs

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+// .storybook/main.js
+
+module.exports = {
+    stories: ['../src/**/*.story.jsx'],
+    addons: [
+        '@storybook/addon-actions',
+        '@storybook/addon-knobs',
+        '@storybook/addon-notes',
+    ],
+}
