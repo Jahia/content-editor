5276243a76d2d527024cd0a88ef4ff58bcc4b05c
Alex Karmanov <alexandrk_@hotmail.com>
2022-04-14 14:26:26 -0400

    BACKLOG-16826 Add ctrl save for creation

--- src/javascript/Create/CreateForm/create.action.js
+++ src/javascript/Create/CreateForm/create.action.js
@@ -5,6 +5,7 @@ import {ComponentRendererContext} from '@jahia/ui-extender';
 import * as PropTypes from 'prop-types';
 import {useFormikContext} from 'formik';
 import {useContentEditorContext} from '~/ContentEditor.context';
+import {useKeydownListener} from '~/utils/getKeydownListener';
 
 const Create = ({render: Render, loading: Loading, ...otherProps}) => {
     const componentRenderer = useContext(ComponentRendererContext);
@@ -12,6 +13,26 @@ const Create = ({render: Render, loading: Loading, ...otherProps}) => {
     const {mode} = useContentEditorContext();
 
     const [clicked, setClicked] = useState(false);
+
+    useKeydownListener(event => {
+        if (event.ctrlKey && event.keyCode === 83) {
+            save();
+        }
+    }, formik.dirty);
+
+    const save = async () => {
+        const formIsValid = await validateForm(formik, componentRenderer);
+
+        if (formIsValid) {
+            setClicked(true);
+            return formik
+                .submitForm()
+                .then(() => {
+                    formik.resetForm({values: formik.values});
+                });
+        }
+    };
+
     if (Loading) {
         return <Loading {...otherProps}/>;
     }
@@ -21,18 +42,7 @@ const Create = ({render: Render, loading: Loading, ...otherProps}) => {
                 addWarningBadge={Object.keys(formik.errors).length > 0}
                 enabled={mode === Constants.routes.baseCreateRoute}
                 disabled={clicked && !formik.dirty}
-                onClick={async () => {
-                    const formIsValid = await validateForm(formik, componentRenderer);
-
-                    if (formIsValid) {
-                        setClicked(true);
-                        return formik
-                            .submitForm()
-                            .then(() => {
-                                formik.resetForm({values: formik.values});
-                            });
-                    }
-                }}/>
+                onClick={save}/>
     );
 };
 
