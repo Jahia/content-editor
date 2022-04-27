0084ad268ecfff354660665f8ac7575f66c4c417
Geofrey Flores <gflores@jahia.com>
2022-04-27 11:37:06 -0400

    Fix lint issues

--- src/javascript/EditPanel/EditPanelDialogConfirmation/EditPanelDialogConfirmation.jsx
+++ src/javascript/EditPanel/EditPanelDialogConfirmation/EditPanelDialogConfirmation.jsx
@@ -21,10 +21,10 @@ const useDialogText = (switchLang, mode) => {
         const isEditMode = mode === Constants.routes.baseEditRoute;
         if (switchLang || !isEditMode) {
             const messageKey = (switchLang) ? 'switchLanguage' : 'message';
-            mode = mode || 'edit';
-            setMessageKey(`${rootProp}.${mode}.${messageKey}`);
+            const modeKey = mode || 'edit';
+            setMessageKey(`${rootProp}.${modeKey}.${messageKey}`);
             if (!isEditMode) {
-                setTitleKey(`${rootProp}.${mode}.title`);
+                setTitleKey(`${rootProp}.${modeKey}.title`);
             }
         }
     }, [switchLang, mode]);
