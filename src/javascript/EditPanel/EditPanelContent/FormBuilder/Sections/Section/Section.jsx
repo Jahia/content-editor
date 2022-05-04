a8d3f8f74418623b4d570f73714c0fd346daf7d0
Alex Karmanov <alexandrk_@hotmail.com>
2022-05-04 14:42:33 -0400

    BACKLOG-17104 Reorganize sections, fix ordering section, fix tests, fix layout issue (#1083)
    * BACKLOG-17140 Reorganize sections, fix ordering section, fix tests, update moonstone
    
    * BACKLOG-17105 Fix warning border

---
+++ src/javascript/EditPanel/EditPanelContent/FormBuilder/Sections/Section/Section.jsx
@@ -0,0 +1,24 @@
+import {Collapsible} from '@jahia/moonstone';
+import FieldSetsDisplay from '~/EditPanel/EditPanelContent/FormBuilder/FieldSet/FieldSetsDisplay/FieldSetsDisplay';
+import {filterRegularFieldSets} from '~/EditPanel/EditPanelContent/FormBuilder/FormBuilder.fieldSetHelp';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {SectionPropTypes} from '~/FormDefinitions';
+
+export const Section = ({section, isExpanded, onClick}) => (
+    <Collapsible data-sel-content-editor-fields-group={section.displayName}
+                 label={section.displayName}
+                 isExpanded={isExpanded}
+                 onClick={onClick}
+    >
+        <FieldSetsDisplay fieldSets={filterRegularFieldSets(section.fieldSets)}/>
+    </Collapsible>
+);
+
+Section.propTypes = {
+    section: SectionPropTypes.isRequired,
+    isExpanded: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired
+};
+
+Section.displayName = 'Section';
