3572870b579777dd9aec96208221f3564658711a
Thomas Draier <tdraier@jahia.com>
2022-05-12 17:42:20 +0200

    Expand section if closed

--- src/javascript/EditPanel/Validation.jsx
+++ src/javascript/EditPanel/Validation.jsx
@@ -2,19 +2,48 @@ import styles from './Validation.scss';
 import React, {useMemo} from 'react';
 import {useFormikContext} from 'formik';
 import {useContentEditorSectionContext} from '~/ContentEditorSection/ContentEditorSection.context';
-import {getFields} from '~/EditPanel/EditPanel.utils';
 import {useTranslation} from 'react-i18next';
+import {useDispatch, useSelector} from 'react-redux';
+import {ceToggleSections} from '~/redux/registerReducer';
+
+function scrollTo(field) {
+    const fieldElement = window.document.querySelector('div[data-sel-content-editor-field="' + field + '"]');
+    window.document.querySelector('#contenteditor-dialog-content').scroll(0, fieldElement.offsetTop);
+}
 
 export const Validation = () => {
     const formik = useFormikContext();
     const {sections} = useContentEditorSectionContext();
     const {t} = useTranslation('content-editor');
-    const fields = useMemo(() => getFields(sections), [sections]);
+    const toggleStates = useSelector(state => state.contenteditor.ceToggleSections);
+    const dispatch = useDispatch();
+    const [fields, sectionsForField] = useMemo(() => {
+        const _sectionsForField = {};
+        const _fields = [];
+        sections.forEach(section => {
+            section.fieldSets.forEach(fieldSet => {
+                fieldSet.fields.forEach(field => {
+                    _fields.push(field);
+                    _sectionsForField[field.name] = section;
+                });
+            });
+        });
+        return [_fields, _sectionsForField];
+    }, [sections]);
+
     const fieldsInError = fields.filter(f => Object.keys(formik.errors).indexOf(f.name) > -1);
 
     const onClick = field => {
-        const fieldElement = window.document.querySelector('div[data-sel-content-editor-field="' + field + '"]');
-        window.document.querySelector('#contenteditor-dialog-content').scroll(0, fieldElement.offsetTop);
+        const section = sectionsForField[field];
+        if (toggleStates[section.name]) {
+            scrollTo(field);
+        } else {
+            dispatch(ceToggleSections({
+                ...toggleStates,
+                [section.name]: true
+            }));
+            setTimeout(() => scrollTo(field), 0);
+        }
     };
 
     return fieldsInError.length > 0 && (
