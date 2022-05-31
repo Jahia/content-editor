ccca5992acdda000700b1f846304500ae967f534
dgaillard <dgaillard.dev@gmail.com>
2022-05-31 14:47:27 +0200

    JS localCompare issue

--- src/javascript/SelectorTypes/Category/category.adapter.js
+++ src/javascript/SelectorTypes/Category/category.adapter.js
@@ -1,4 +1,11 @@
 export const adaptToCategoryTree = ({nodes, parent, selectedValues, locale}) => {
+
+    // This is only to raise the issue, you will most likely want to fix it somewhere else and do something nicer as well.
+    // localeCompare in JS expect a locale like en-US NOT en_US which Jahia uses.
+    if (locale.indexOf("_") !== -1) {
+        locale = locale.replace("_", "-");
+    }
+
     return nodes
         .filter(category => category.parent.uuid === parent.uuid)
         .map(category => {
