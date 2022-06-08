188ca7d4dbdf2606f356dec5355b003784d16c16
Cedric Mailleux <cmailleux@jahia.com>
2022-06-08 11:53:12 -0400

    BACKLOG-16840: Add Cypress test framework in CE 4.0 (#1142)
    * BACKLOG-16840: Add cypress tests
    
    * BACKLOG-16840: Add first test
    
    * BACKLOG-16840: Add retries to wait for Jahia first compilation of JSP pages
    
    * BACKLOG-16840: Refactor test to use page object (PageComposer and ContentEditor)
    
    * BACKLOG-16840: Use latest jahia/cypress
    
    * BACKLOG-16840: Add create multiple content test
    
    * BACKLOG-16840: Add work in progress test
    
    * BACKLOG-16840: Add create content in I18N context tests
    
    * BACKLOG-16840: Add iframe loading timeout
    
    * BACKLOG-16840: use cypress image with browsers
    
    * BACKLOG-16840: start chrome
    
    * BACKLOG-16840: fix run in e2e:ci by unchecking Create another before saving to avoid discard popup
    
    * BACKLOG-16840: upgrade to cypress 10.0.2
    
    * BACKLOG-16840: use focused and framelaoded when switching language
    
    * BACKLOG-16840: Add edit newly created content test

---
+++ tests/cypress/page-object/contentEditor.ts
@@ -0,0 +1,85 @@
+import {
+    BasePage,
+    Button,
+    Collapsible,
+    Dropdown, getComponentByAttr,
+    getComponentByRole,
+    getComponentBySelector,
+    Menu,
+} from '@jahia/cypress'
+import {PageComposer} from './pageComposer'
+
+export class ContentEditor extends BasePage {
+    languageSwitcher: Dropdown
+
+    static visit(site: string, language: string, path: string): ContentEditor {
+        cy.visit(`/jahia/page-composer/default/${language}/sites/${site}/${path}`)
+        return new ContentEditor()
+    }
+
+    getPageComposer(): PageComposer {
+        return new PageComposer(this)
+    }
+
+    openSection(sectionName: string) {
+        return getComponentBySelector(Collapsible, `[data-sel-content-editor-fields-group="${sectionName}"]`).expand()
+    }
+
+    closeSection(sectionName: string) {
+        return getComponentBySelector(Collapsible, `[data-sel-content-editor-fields-group="${sectionName}"]`).collapse()
+    }
+
+    save() {
+        getComponentByRole(Button, 'createButton').click()
+        cy.get('[role="alertdialog"]').should('be.visible').should('contain', 'Content successfully created')
+    }
+
+    editSavedContent() {
+        cy.get('[role="alertdialog"]').should('be.visible').find('.moonstone-button').click()
+    }
+
+    cancel() {
+        getComponentByRole(Button, 'backButton').click()
+    }
+
+    addAnotherContent() {
+        cy.get('#createAnother').check()
+    }
+
+    removeAnotherContent() {
+        cy.get('#createAnother').uncheck()
+    }
+
+    activateWorkInProgressMode(language?: string) {
+        if (language === undefined) {
+            getComponentByRole(Button, '3dotsMenuAction').click()
+            getComponentBySelector(Menu, '#menuHolder').selectByRole('goToWorkInProgress')
+            cy.get('[data-sel-role="wip-info-chip"]').should('contain', 'Work in progress')
+        } else if (language === 'ALL') {
+            //Activate all properties
+            getComponentByRole(Button, '3dotsMenuAction').click()
+            getComponentBySelector(Menu, '#menuHolder').selectByRole('goToWorkInProgress')
+            cy.get('[data-sel-role="WIP"]').click()
+            cy.get('input[type="radio"]').filter('input[value="ALL_CONTENT"]').click()
+            cy.get('.moonstone-button').filter(':contains("Done")').click()
+            cy.get('[data-sel-role="wip-info-chip"]').should('contain', 'Work in progress')
+        } else {
+            //Activate all properties
+            getComponentByRole(Button, '3dotsMenuAction').click()
+            getComponentBySelector(Menu, '#menuHolder').selectByRole('goToWorkInProgress')
+            cy.get('[data-sel-role="WIP"]').click()
+            language.split(',').forEach((value) => {
+                cy.get('input[type="checkbox"]').check(value)
+            })
+            cy.get('.moonstone-button').filter(':contains("Done")').click()
+            cy.get('[data-sel-role="wip-info-chip"]').should('contain', 'WIP - EN')
+        }
+    }
+
+    getLanguageSwitcher(): Dropdown {
+        if (!this.languageSwitcher) {
+            this.languageSwitcher = getComponentByAttr(Dropdown, 'data-cm-role', 'language-switcher')
+        }
+        return this.languageSwitcher
+    }
+}
---
+++ tests/cypress/page-object/pageComposer.ts
@@ -0,0 +1,73 @@
+import { BaseComponent, BasePage, Button, getComponent, getComponentByRole, getElement, MUIInput } from '@jahia/cypress'
+import { ContentEditor } from './contentEditor'
+import IframeOptions = Cypress.IframeOptions
+
+export class PageComposer extends BasePage {
+    jcontent: ContentEditor
+    iFrameOptions: IframeOptions
+    constructor(jcontent: ContentEditor) {
+        super()
+        this.jcontent = jcontent
+        this.iFrameOptions = { timeout: 90000, log: true }
+    }
+
+    openCreateContent(): PageComposer {
+        cy.iframe('#page-composer-frame', this.iFrameOptions).within(() => {
+            cy.iframe('.gwt-Frame', this.iFrameOptions).within(() => {
+                cy.get('.container').contains('Any content').click()
+            })
+        })
+        return this
+    }
+
+    refresh() {
+        cy.iframe('#page-composer-frame', this.iFrameOptions).within(() => {
+            cy.get('.window-actions-refresh').click()
+        })
+        return this
+    }
+
+    shouldContain(text: string) {
+        cy.iframe('#page-composer-frame', this.iFrameOptions).within(() => {
+            cy.iframe('.gwt-Frame', this.iFrameOptions).within(() => {
+                cy.get('.container').should('contain', text)
+            })
+        })
+    }
+
+    getContentTypeSelector(): ContentTypeSelector {
+        return getComponent(ContentTypeSelector)
+    }
+
+    shouldContainWIPOverlay() {
+        cy.iframe('#page-composer-frame', this.iFrameOptions).within(() => {
+            cy.iframe('.gwt-Frame', this.iFrameOptions).within(() => {
+                cy.get('.workinprogress-overlay').should('contain', 'Work in progress')
+            })
+        })
+    }
+}
+
+export class ContentTypeSelector extends BaseComponent {
+    static defaultSelector = 'div[aria-labelledby="dialog-createNewContent"]'
+
+    searchInput = getComponentByRole(MUIInput, 'content-type-dialog-input', this)
+
+    searchForContentType(contentType: string): ContentTypeSelector {
+        this.searchInput.type(contentType)
+        return this
+    }
+
+    selectContentType(contentType: string): ContentTypeSelector {
+        getElement('[data-sel-role="content-type-tree"] span', this).contains(contentType).click()
+        return this
+    }
+
+    cancel(): void {
+        getComponentByRole(Button, 'content-type-dialog-cancel', this).click()
+    }
+
+    create(): void {
+        getComponentByRole(Button, 'content-type-dialog-create', this).click()
+    }
+}
