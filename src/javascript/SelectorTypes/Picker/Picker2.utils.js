5ffb0b9c5044c9582f92b1a1c44037c74104e8c1
Thomas Draier <tdraier@jahia.com>
2022-07-05 10:57:56 +0200

    merge issues

--- src/javascript/SelectorTypes/Picker/Picker2.utils.js
+++ src/javascript/SelectorTypes/Picker/Picker2.utils.js
@@ -1,21 +1,4 @@
-import {registry} from '@jahia/ui-extender';
 import {Constants} from './Picker2.constants';
-
-export const extractConfigs = field => {
-    // Resolve picker configuration
-    const pickerConfig = _resolveConfig(
-        field.selectorOptions,
-        field
-    );
-
-    return {
-        pickerConfig: {
-            ...pickerConfig,
-            displayTree: pickerConfig.displayTree !== false,
-            pickerType: _getPickerType(field.selectorOptions)
-        }
-    };
-};
 
 export const getPathWithoutFile = fullPath => {
     return fullPath && fullPath.split('/').slice(0, -1).join('/');
@@ -54,25 +37,3 @@ export const getAccordionMode = fullPath => {
         default: return Constants.ACCORDION_ITEM_TYPES.PAGES;
     }
 };
-
-const _getPickerType = options => {
-    const pickerOption = options && options.find(option => option.name === 'type' && registry.get('pickerConfiguration', option.value));
-    return (pickerOption && pickerOption.value) || 'editorial';
-};
-
-export const _resolveConfig = (options, field) => {
-    const config = Object.assign({}, registry.get('pickerConfiguration', _getPickerType(options)).cmp);
-    if (field && field.valueConstraints) {
-        const constraints = field.valueConstraints.map(constraint => constraint.value.string);
-        if (constraints && constraints.length > 0) {
-            config.selectableTypesTable = constraints;
-        }
-    }
-
-    return config;
-};
-
-export const getPickerSelectorType = options => {
-    return registry.get('pickerConfiguration', _getPickerType(options)).cmp.picker;
-};
-
