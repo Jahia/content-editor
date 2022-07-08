7f5a0e853d44db5874aea8ea43fc481c8a31b0a9
Thomas Draier <tdraier@jahia.com>
2022-07-08 17:59:55 +0200

    Fixed proptype

--- src/javascript/SelectorTypes/Picker/configs/configPropType.js
+++ src/javascript/SelectorTypes/Picker/configs/configPropType.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
 export const configPropType = PropTypes.shape({
-    pickerInput: {
+    pickerInput: PropTypes.shape({
         emptyLabel: PropTypes.string.isRequired,
         notFoundLabel: PropTypes.string.isRequired,
         emptyIcon: PropTypes.node.isRequired,
         usePickerInputData: PropTypes.func
-    },
+    }),
     pickerDialog: PropTypes.shape({
         view: PropTypes.string.isRequired,
         dialogTitle: PropTypes.string.isRequired,
