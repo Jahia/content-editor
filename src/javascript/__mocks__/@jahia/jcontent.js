3de942f0cf1e5ef9a0f4cb19beaeb86c6d1eacec
Alex Karmanov <alexandrk_@hotmail.com>
2022-07-27 14:31:28 -0400

    BACKLOG-17572 Fix tests

---
+++ src/javascript/__mocks__/@jahia/jcontent.js
@@ -0,0 +1,8 @@
+export const ContentTree = jest.fn();
+export const reactTable = {
+    CellPublicationStatus: jest.fn(),
+    CellName: jest.fn(),
+    Header: jest.fn(),
+    CellType: jest.fn(),
+    CellLastModified: jest.fn()
+}
