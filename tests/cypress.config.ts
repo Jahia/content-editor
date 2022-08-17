f91b936c83fd8a7d90aedf49ec6e18ee56e317bd
Cedric Mailleux <cmailleux@jahia.com>
2022-08-17 16:14:23 -0400

    BACKLOG-19920: Boost default timeout

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
     chromeWebSecurity: false,
-    defaultCommandTimeout: 2000,
+    defaultCommandTimeout: 90000,
     videoUploadOnPasses: false,
     reporter: 'cypress-multi-reporters',
     reporterOptions: {
