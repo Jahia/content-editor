c019423cd526174a93ad4786bf915e48b2150753
Thomas Draier <tdraier@jahia.com>
2022-08-25 19:31:58 +0200

    use fileSize from jcontent

--- src/javascript/SelectorTypes/Picker/configs/mediaPicker/FilePickerCaption.js
+++ src/javascript/SelectorTypes/Picker/configs/mediaPicker/FilePickerCaption.js
@@ -1,9 +1,8 @@
 import clsx from 'clsx';
 import styles from '~/SelectorTypes/Picker/PickerDialog/RightPanel/PickerSelection/Selection';
 import {Typography} from '@jahia/moonstone';
-import bytes from 'bytes';
 import React from 'react';
-import {NodeIcon} from '@jahia/jcontent';
+import {FileSize, NodeIcon} from '@jahia/jcontent';
 import PropTypes from 'prop-types';
 
 export const FilePickerCaption = ({selection}) => (
@@ -11,8 +10,7 @@ export const FilePickerCaption = ({selection}) => (
         <div className={clsx('flexRow_nowrap', 'alignCenter', styles.text)} data-sel-role="item-selected">
             <NodeIcon node={selection}/>
             <Typography isNowrap variant="body">{selection.displayName}</Typography>
-            <Typography className={styles.gray}
-                        variant="body"> - {bytes(selection.children.nodes[0]?.data?.size, {unitSeparator: ' '})}</Typography>
+            <Typography className={styles.gray} variant="body"> - <FileSize node={selection}/></Typography>
         </div>
         <Typography className={styles.gray} variant="caption">{selection.path}</Typography>
     </>
