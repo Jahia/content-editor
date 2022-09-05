02c949c9bebe81209bbd012dd96b8a75c3d6045b
Thomas Draier <tdraier@jahia.com>
2022-09-05 11:46:45 +0200

    BACKLOG-19977 Auto expand 3 levels

--- src/javascript/SelectorTypes/Picker/PickerDialog/RightPanel/ContentLayout/ContentLayout.container.jsx
+++ src/javascript/SelectorTypes/Picker/PickerDialog/RightPanel/ContentLayout/ContentLayout.container.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import {useTranslation} from 'react-i18next';
-import {shallowEqual, useSelector} from 'react-redux';
+import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import {Loader} from '@jahia/moonstone';
 import {Constants} from '~/SelectorTypes/Picker/Picker2.constants';
 import {configPropType} from '~/SelectorTypes/Picker/configs/configPropType';
@@ -9,6 +9,7 @@ import {registry} from '@jahia/ui-extender';
 import {useLayoutQuery} from '@jahia/jcontent';
 import clsx from 'clsx';
 import styles from './ContentLayout.scss';
+import {cePickerOpenPaths} from '~/SelectorTypes/Picker/Picker2.redux';
 
 const setRefetcher = (name, refetcherData) => {
     registry.addOrReplace('refetcher', name, refetcherData);
@@ -21,15 +22,19 @@ const unsetRefetcher = name => {
 export const ContentLayoutContainer = ({pickerConfig}) => {
     const {t} = useTranslation();
     const currentResult = useRef();
-    const {mode, path, filesMode, preSearchModeMemo, searchTerms, searchPath} = useSelector(state => ({
+    const {mode, path, filesMode, preSearchModeMemo, viewType, searchTerms, searchPath} = useSelector(state => ({
         mode: state.contenteditor.picker.mode,
         path: state.contenteditor.picker.path,
         filesMode: 'grid',
         tableView: state.contenteditor.picker.tableView,
         preSearchModeMemo: state.contenteditor.picker.preSearchModeMemo,
+        viewType: state.contenteditor.picker.tableView.viewType,
         searchTerms: state.contenteditor.picker.searchTerms,
         searchPath: state.contenteditor.picker.searchPath
     }), shallowEqual);
+
+    const dispatch = useDispatch();
+
     const openableTypes = registry.get('accordionItem', mode)?.config?.openableTypes;
 
     const additionalFragments = [];
@@ -66,6 +71,17 @@ export const ContentLayoutContainer = ({pickerConfig}) => {
             unsetRefetcher('pickerData');
         };
     });
+
+    const autoExpand = useRef({path: '', level: 1, type: ''});
+    useEffect(() => {
+        if (isStructured && !loading && result?.nodes?.length && (autoExpand.current.path !== path || autoExpand.current.type !== viewType || autoExpand.current.level < 2)) {
+            autoExpand.current.level = (autoExpand.current.path === path && autoExpand.current.type === viewType) ? autoExpand.current.level + 1 : 1;
+            autoExpand.current.path = path;
+            autoExpand.current.type = viewType;
+            dispatch(cePickerOpenPaths(result.nodes.flatMap(r => [r.path, ...r.subRows?.map(c => c.path)])));
+        }
+    }, [dispatch, result, isStructured, path, viewType, loading, autoExpand]);
+
     if (error || (!loading && !result)) {
         if (error) {
             const message = t('jcontent:label.contentManager.error.queryingContent', {details: error.message || ''});
