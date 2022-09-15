ce5357f789fa920f2d395de9fc1bf7855754b2f4
Cedric Mailleux <cmailleux@jahia.com>
2022-09-15 14:15:41 -0400

    BACKLOG-17390: Modify openInTabAction to be working either from picker field or form actions menu (#1303)
    * BACKLOG-17390: Modify openInTabAction to be working either from picker field or form actions menu
    
    * BACKLOG-17390: fix unit test

--- src/javascript/SelectorTypes/Picker/actions/openInTabAction.js
+++ src/javascript/SelectorTypes/Picker/actions/openInTabAction.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Constants} from '~/ContentEditor.constants';
-import {shallowEqual, useSelector} from 'react-redux';
+import {useContentEditorContext} from '~/contexts';
+import {useQuery} from '@apollo/react-hooks';
+import {OpenInTabActionQuery} from '~/SelectorTypes/Picker/actions/openInTabAction.gql-queries';
 
-export const OpenInTabActionComponent = ({render: Render, loading: Loading, uuid, ...others}) => {
-    const {lang} = useSelector(state => ({
-        lang: state.language
-    }), shallowEqual);
+export const OpenInTabActionComponent = ({render: Render, loading: Loading, path, field, inputContext, ...others}) => {
+    const {lang} = useContentEditorContext();
+
+    let uuid;
+    if (path === undefined) {
+        const {fieldData} = inputContext.actionContext;
+        uuid = fieldData[0].uuid;
+    }
+
+    const {data, error, loading} = useQuery(OpenInTabActionQuery, {
+        variables: {
+            path: path
+        },
+        skip: !path
+    });
+
+    if (uuid === undefined && (loading || error || !data)) {
+        return <></>;
+    }
+
+    uuid = uuid === undefined ? data.jcr.result.uuid : uuid;
 
     return (
         <Render
@@ -19,11 +38,11 @@ export const OpenInTabActionComponent = ({render: Render, loading: Loading, uuid
 };
 
 OpenInTabActionComponent.propTypes = {
-    uuid: PropTypes.string,
-
+    path: PropTypes.string,
     render: PropTypes.func.isRequired,
-
-    loading: PropTypes.func
+    loading: PropTypes.func,
+    field: PropTypes.object,
+    inputContext: PropTypes.object
 };
 
 export const openInTabAction = {
