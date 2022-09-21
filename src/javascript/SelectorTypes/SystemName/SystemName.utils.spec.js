fe87a0b79d3496d0d68ca46388828f17910e8bfb
Thomas Draier <tdraier@jahia.com>
2022-09-21 18:23:39 +0200

    Fixed test

--- src/javascript/SelectorTypes/SystemName/SystemName.utils.spec.js
+++ src/javascript/SelectorTypes/SystemName/SystemName.utils.spec.js
@@ -15,13 +15,13 @@ describe('SystemName utils', () => {
     });
 
     it('should be limited to 32 characters when have selector options', () => {
-        const systemNameValue = limitSystemNameIfNecessary('1234567890123456789012345678901234567890', field);
+        const systemNameValue = replaceSpecialCharacters('1234567890123456789012345678901234567890', field);
         expect(systemNameValue).toBe('12345678901234567890123456789012');
     });
 
     it('should not be limited when have no maxlength selector option', () => {
         field.selectorOptions = [];
-        const systemNameValue = limitSystemNameIfNecessary('1234567890123456789012345678901234567890', field);
+        const systemNameValue = replaceSpecialCharacters('1234567890123456789012345678901234567890', field);
 
         expect(systemNameValue).toBe('1234567890123456789012345678901234567890');
     });
