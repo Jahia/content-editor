42446f95f4167f90092262bd537db490e854187a
Thomas Draier <tdraier@jahia.com>
2022-11-10 20:30:51 +0100

    BACKLOG-20236: Adjust form styles (#1394)
    * BACKLOG-20236: Adjust form styles
    
    * tests

--- src/javascript/editorTabs/EditPanelContent/FormBuilder/FieldSet/FieldSet.jsx
+++ src/javascript/editorTabs/EditPanelContent/FormBuilder/FieldSet/FieldSet.jsx
@@ -5,90 +5,50 @@ import {FieldSetPropTypes} from '~/ContentEditor.proptypes';
 import {FieldContainer} from '../Field';
 import {useFormikContext} from 'formik';
 import styles from './FieldSet.scss';
-import clsx from 'clsx';
-
-const DynamicFieldSet = ({fieldset}) => {
-    const {values, handleChange} = useFormikContext();
-    const activatedFieldSet = (values && values[fieldset.name]);
-    return (
-        <article>
-            {!fieldset.hideHeader && (
-                <div className={styles.fieldsetTitleContainer}>
-                    <div className="flexRow alignCenter">
-                        <Toggle
-                            classes={{
-                                switchBase: styles.toggle,
-                                disabledSwitchBase: styles.toggle,
-                                readOnlySwitchBase: styles.toggle,
-                                focusedSwitchBase: styles.toggle
-                            }}
-                            data-sel-role-dynamic-fieldset={fieldset.name}
-                            id={fieldset.name}
-                            checked={activatedFieldSet}
-                            readOnly={fieldset.readOnly}
-                            onChange={handleChange}
-                        />
-                        <Typography component="label"
-                                    htmlFor={fieldset.name}
-                                    className={styles.fieldSetTitle}
-                                    variant="subheading"
-                        >
-                            {fieldset.displayName}
-                        </Typography>
-                    </div>
-                    {fieldset.description && (
-                        <Typography component="label" className={styles.fieldSetDescription} variant="caption">
-                            {fieldset.description}
-                        </Typography>
-                    )}
-                </div>
-            )}
-
-            {activatedFieldSet && fieldset.fields.map(field => <FieldContainer key={field.name} field={field}/>)}
-        </article>
-    );
-};
-
-DynamicFieldSet.propTypes = {
-    fieldset: FieldSetPropTypes.isRequired
-};
-
-const StaticFieldSet = ({fieldset}) => {
-    return (
-        <article>
-            {!fieldset.hideHeader && (
-                <div className={styles.fieldsetTitleContainer}>
-                    <div className="flexRow alignCenter">
-                        <Typography component="label"
-                                    htmlFor={fieldset.name}
-                                    className={styles.fieldSetTitle}
-                                    variant="subheading"
-                        >
-                            {fieldset.displayName}
-                        </Typography>
-                    </div>
-                    {fieldset.description && (
-                        <Typography component="label" className={clsx(styles.fieldSetDescription, styles.staticFieldSetDescription)} variant="caption">
-                            {fieldset.description}
-                        </Typography>
-                    )}
-                </div>
-            )}
-
-            {fieldset.fields.map(field => <FieldContainer key={field.name} field={field}/>)}
-        </article>
-    );
-};
-
-StaticFieldSet.propTypes = {
-    fieldset: FieldSetPropTypes.isRequired
-};
 
 export const FieldSet = ({fieldset}) => {
-    return fieldset.dynamic ? (
-        <DynamicFieldSet fieldset={fieldset}/>
-    ) : (
-        <StaticFieldSet fieldset={fieldset}/>
+    const {values, handleChange} = useFormikContext();
+    const activatedFieldSet = !fieldset.dynamic || (values && values[fieldset.name]);
+
+    return (
+        <article className={activatedFieldSet && fieldset.fields.length > 0 ? styles.fieldSetOpen : styles.fieldSet}>
+            {!fieldset.hideHeader && (
+                <div className={styles.fieldSetTitleContainer}>
+                    <div className="flexRow">
+                        {fieldset.dynamic && (
+                            <Toggle
+                                classes={{
+                                    root: styles.toggle
+                                }}
+                                data-sel-role-dynamic-fieldset={fieldset.name}
+                                id={fieldset.name}
+                                checked={activatedFieldSet}
+                                readOnly={fieldset.readOnly}
+                                onChange={handleChange}
+                            />
+                        )}
+                        <div className="flexCol">
+                            <Typography component="label"
+                                        htmlFor={fieldset.name}
+                                        className={styles.fieldSetTitle}
+                                        variant="subheading"
+                                        weight="bold"
+                            >
+                                {fieldset.displayName}
+                            </Typography>
+                            {fieldset.description && (
+                                <Typography component="label" className={styles.fieldSetDescription} variant="caption">
+                                    {fieldset.description}
+                                </Typography>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
+            <div className={styles.fields}>
+                {activatedFieldSet && fieldset.fields.map(field => <FieldContainer key={field.name} field={field}/>)}
+            </div>
+        </article>
     );
 };
 
