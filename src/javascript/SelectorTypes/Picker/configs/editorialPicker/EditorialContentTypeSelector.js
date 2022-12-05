608d2abf5aabdccc3e27dea7695ba0dc525c1882
Thomas Draier <tdraier@jahia.com>
2022-12-05 15:29:26 +0100

    Upgrade libs

--- src/javascript/SelectorTypes/Picker/configs/editorialPicker/EditorialContentTypeSelector.js
+++ src/javascript/SelectorTypes/Picker/configs/editorialPicker/EditorialContentTypeSelector.js
@@ -31,5 +31,5 @@ export const EditorialContentTypeSelector = ({pickerConfig}) => {
 };
 
 EditorialContentTypeSelector.propTypes = {
-    pickerConfig: configPropType.isRequired
+    pickerConfig: configPropType
 };
