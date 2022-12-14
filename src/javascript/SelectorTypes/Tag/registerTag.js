e70940ad9de91894d9a322c717b484905078872a
jsinovassin <jsinovassinnaik@jahia.com>
2022-12-14 18:22:14 +0100

    BACKLOG-20462 : add icon, display name, description for selector types

--- src/javascript/SelectorTypes/Tag/registerTag.js
+++ src/javascript/SelectorTypes/Tag/registerTag.js
@@ -1,5 +1,13 @@
 import {Tag} from './Tag';
 
 export const registerTag = ceRegistry => {
-    ceRegistry.add('selectorType', 'Tag', {dataType: ['String'], cmp: Tag, supportMultiple: true});
+    ceRegistry.add('selectorType', 'Tag', {
+        dataType: ['String'],
+        displayValue: 'content-editor:label.contentEditor.selectorTypes.tag.displayValue',
+        properties: [
+            {name: 'description', value: 'content-editor:label.contentEditor.selectorTypes.tag.description'},
+            {name: 'iconStart', value: 'Label'}
+        ],
+        cmp: Tag, supportMultiple: true
+    });
 };
