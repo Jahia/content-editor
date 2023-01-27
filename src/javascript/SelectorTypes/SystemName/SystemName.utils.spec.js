d2e6ad3fddd3de2d849800913cb272079f1f0b44
Thomas Draier <tdraier@jahia.com>
2023-01-27 10:30:35 +0100

    test

--- src/javascript/SelectorTypes/SystemName/SystemName.utils.spec.js
+++ src/javascript/SelectorTypes/SystemName/SystemName.utils.spec.js
@@ -28,7 +28,7 @@ describe('SystemName utils', () => {
 
     it('should replace every special character when present', () => {
         const newSystemName = replaceSpecialCharacters('-ffee_ek,veéèëÊË€éèëÊË€§çÇ¢ùÙÛàÅåÁª¶øôÔØÓ°£™Ÿ¥‰îïÎÍŒÆ$.+*');
-        expect(newSystemName).toBe('ffee_ek,veeeeeeeuroeeeeeeuro-cccentuuuaaaaa-ooooo-poundTMyyen-iiiioeaedollar');
+        expect(newSystemName).toBe('ffee_ek-veeeeee-eeeee-cc-uuuaaaaa-ooooo-TMy-iiiioeae');
     });
 
     it('should be equal to system name', () => {
