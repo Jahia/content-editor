7fcb573b7af5a886ea2112fca145429b533fba2f
Cedric Mailleux <cmailleux@jahia.com>
2023-04-20 14:52:47 -0400

    QA-14483: Use mimeType ig available to figure file type, icon and preview (#1601)
    * QA-14483: Use mimeType ig available to figure file type, icon and preview
    
    * QA-14483: Update to data helper 1.0.8

--- src/javascript/editorTabs/EditPanelContent/Preview/Preview.utils.js
+++ src/javascript/editorTabs/EditPanelContent/Preview/Preview.utils.js
@@ -103,23 +103,48 @@ export const forceDisplay = element => {
     }
 };
 
-export const isBrowserImage = function (filename) {
-    switch (filename.split('.').pop().toLowerCase()) {
-        case 'png':
-        case 'jpeg':
-        case 'jpg':
-        case 'gif':
-        case 'img':
-        case 'svg':
-        case 'bmp':
-            return true;
-        default:
-            return false;
+export const isBrowserImage = function (node) {
+    if (node.isFile) {
+        let mimetype = node.content !== undefined ? node.content.mimeType.value : node.resourceChildren.nodes[0].mimeType.value;
+        if (mimetype === 'application/binary' || mimetype === 'application/octet-stream') {
+            switch (node.path.split('.').pop().toLowerCase()) {
+                case 'avif':
+                case 'png':
+                case 'jpeg':
+                case 'jpg':
+                case 'gif':
+                case 'svg':
+                case 'img':
+                case 'webp':
+                case 'bmp':
+                    return true;
+                default:
+                    return false;
+            }
+        } else {
+            return mimetype.startsWith('image/');
+        }
     }
+
+    return false;
 };
 
-export const isPDF = function (filename) {
-    return filename.split('.').pop().toLowerCase() === 'pdf';
+export const isPDF = function (node) {
+    if (node.isFile) {
+        let mimetype = node.content !== undefined ? node.content.mimeType.value : node.resourceChildren.nodes[0].mimeType.value;
+        if (mimetype === 'application/binary' || mimetype === 'application/octet-stream') {
+            switch (node.path.split('.').pop().toLowerCase()) {
+                case 'pdf':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        return mimetype.toLowerCase().indexOf('pdf') > 0;
+    }
+
+    return false;
 };
 
 export const getFileType = function (filename) {
