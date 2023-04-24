432824e2c49e7d2d110241ac5d0878f0d0768a34
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-04-24 13:34:49 +0200

    fix date format

--- tests/cypress/e2e/pickers/date.cy.ts
+++ tests/cypress/e2e/pickers/date.cy.ts
@@ -1,8 +1,8 @@
-import { 
-    createSite, 
-    createUser, 
-    deleteSite, 
-    deleteUser, 
+import {
+    createSite,
+    createUser,
+    deleteSite,
+    deleteUser,
     grantRoles,
     addNode,
     getNodeByPath,
@@ -12,13 +12,13 @@ import {
 const getDate = (separator: string): string => {
     const date = new Date();
     const day = date.getDate();
-    let month = date.getMonth() + 1;
+    const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const trueDay = day < 10 ? '0' + day : day;
     const trueMonth = month < 10 ? '0' + month : month;
-    
-    return `${trueDay}${separator}${trueMonth}${separator}${year}`
-}
+
+    return `${trueMonth}${separator}${trueDay}${separator}${year}`;
+};
 
 describe('Date picker tests', () => {
     before('Create required content', () => {
@@ -29,23 +29,23 @@ describe('Date picker tests', () => {
             parentPathOrId: '/sites/testsite/contents',
             name: 'contentEditorTestContents',
             primaryNodeType: 'jnt:contentFolder'
-        })
+        });
         addNode({
             parentPathOrId: '/sites/testsite/contents',
             name: 'contentEditorPickers',
             primaryNodeType: 'qant:pickers'
-        })
+        });
     });
 
     beforeEach('Check datePicker is empty', () => {
         cy.login();
         getNodeByPath('/sites/testsite/contents/contentEditorPickers')
-        .its('data.jcr.nodeByPath.uuid').as('datePicker');
-        cy.get('@datePicker').then((uuid) => {
+            .its('data.jcr.nodeByPath.uuid').as('datePicker');
+        cy.get('@datePicker').then(uuid => {
             cy.visit(`/jahia/content-editor/en/edit/${uuid}`);
-        })
+        });
         cy.get('input[id="qant:pickers_datepicker"]').should('have.value', '');
-    })
+    });
 
     it('Test Date Picker', () => {
         cy.get('input[id="qant:pickers_datepicker"]').parent().find('button').click();
@@ -54,24 +54,24 @@ describe('Date picker tests', () => {
 
     it('Test without using picker', () => {
         cy.get('input[id="qant:pickers_datepicker"]').type(getDate(''));
-    })
+    });
 
     afterEach('Check Value is kept after saving and clean picker', () => {
         cy.get('body').click();
         cy.get('input[id="qant:pickers_datepicker"]').should('have.value', getDate('/'));
         cy.get('span').contains('SAVE').click();
-        cy.get('@datePicker').then((uuid) => {
+        cy.get('@datePicker').then(uuid => {
             cy.visit(`/jahia/content-editor/en/edit/${uuid}`);
-        })
+        });
         cy.get('input[id="qant:pickers_datepicker"]').should('have.value', getDate('/'));
         deleteNodeProperty('/sites/testsite/contents/contentEditorPickers', 'datepicker', 'en');
-        cy.reload()
+        cy.reload();
         cy.get('input[id="qant:pickers_datepicker"]').should('have.value', '');
         cy.logout();
-    })
+    });
 
     after('Remove tests content', () => {
         deleteUser('myUser');
         deleteSite('testsite');
     });
-})
+});
