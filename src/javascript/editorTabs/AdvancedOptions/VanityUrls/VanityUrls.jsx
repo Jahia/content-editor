05467b6b62df5db29535e8ae2621b7df2ae92449
jahia-carp <carp@jahia.com>
2023-05-02 18:22:34 +0200

    BACKLOG-20684: Remove access to legacy UI for vanity URLs

--- src/javascript/editorTabs/AdvancedOptions/VanityUrls/VanityUrls.jsx
+++ src/javascript/editorTabs/AdvancedOptions/VanityUrls/VanityUrls.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {useTranslation} from 'react-i18next';
 import styles from './VanityUrls.scss';
 import {getButtonRenderer} from '~/utils';
@@ -7,33 +6,24 @@ import {DisplayAction} from '@jahia/ui-extender';
 import {Typography} from '@jahia/moonstone';
 import clsx from 'clsx';
 
-const ButtonRenderer = getButtonRenderer({defaultButtonProps: {variant: 'default', size: 'big', color: 'default'}});
 const ButtonRendererAccent = getButtonRenderer({defaultButtonProps: {variant: 'default', size: 'big', color: 'accent'}});
 
-export const VanityUrls = ({tab, actionKey}) => {
+export const VanityUrls = () => {
     const {t} = useTranslation('content-editor');
 
     return (
         <section className={styles.container}>
             <Typography variant="heading" className={clsx(styles.item, styles.capitalize)}>
-                {tab} - {t('content-editor:label.contentEditor.gwtTabsDeprecation.title')}
+                {t('content-editor:label.contentEditor.vanityTab.title')}
             </Typography>
             <Typography className={styles.item}>
-                {t(`content-editor:label.contentEditor.gwtTabsDeprecation.warnings.${tab}`)}
+                {t('content-editor:label.contentEditor.vanityTab.label')}
             </Typography>
             <div className={styles.item}>
-                <DisplayAction actionKey={actionKey}
-                               render={props => <ButtonRenderer buttonProps={{className: styles.marginRight, label: t('content-editor:label.contentEditor.gwtTabsDeprecation.openLegacy')}} {...props}/>}
-                />
                 <DisplayAction actionKey="vanityUrls"
                                render={ButtonRendererAccent}
                 />
             </div>
         </section>
     );
-};
-
-VanityUrls.propTypes = {
-    tab: PropTypes.string.isRequired,
-    actionKey: PropTypes.string.isRequired
 };
