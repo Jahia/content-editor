d6aaad899b02adf5fc7cda4d7ca1d3226edf7673
akarmanov <alexandrk_@hotmail.com>
2023-06-12 17:45:35 -0400

    QA-14872 Modify  readonly condition

--- src/main/java/org/jahia/modules/contenteditor/api/forms/impl/EditorFormServiceImpl.java
+++ src/main/java/org/jahia/modules/contenteditor/api/forms/impl/EditorFormServiceImpl.java
@@ -500,8 +500,8 @@ public class EditorFormServiceImpl implements EditorFormService {
         Pattern pathPattern = Pattern.compile("^/sites/[^/]*/(contents|files)$");
         if  (currentNode.isNodeType("jmix:systemNameReadonly")
             || readOnlyNodeTypes.contains(primaryNodeType.getName())
-            // || pathPattern.matcher(currentNode.getPath()).matches()
-            || (!CREATE.equals(mode) && !currentNode.hasPermission("jcr:modifyProperties_default_" + locale.getLanguage()))
+            || (EDIT.equals(mode) && pathPattern.matcher(currentNode.getPath()).matches())
+            || (EDIT.equals(mode) && !currentNode.hasPermission("jcr:modifyProperties_default_" + locale.getLanguage()))
             || JCRContentUtils.isLockedAndCannotBeEdited(currentNode)) {
             systemNameField.setReadOnly(true);
         } else {
