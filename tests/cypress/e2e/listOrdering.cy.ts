1a4e02546f2aeb3fbc570f097d333345ca2389f4
Alex Karmanov <alexandrk_@hotmail.com>
2023-07-13 11:07:09 -0400

    Revert "BACKLOG-21238 Remove ce:manualOrdering field (#1665)" (#1672)
    This reverts commit 68b6ca3193e3707007d5b6460597e046e7473c5f.

--- tests/cypress/e2e/listOrdering.cy.ts
+++ tests/cypress/e2e/listOrdering.cy.ts
@@ -1,11 +1,9 @@
 import {JContent} from '../page-object/jcontent';
 import {Collapsible, getComponentBySelector} from '@jahia/cypress';
-import {ContentEditor} from '../page-object';
 
 describe('Test list ordering', {retries: 0}, () => {
     const siteKey = 'digitall';
     let jcontent: JContent;
-    let contentEditor: ContentEditor;
 
     beforeEach(() => {
         cy.login(); // Edit in chief
@@ -20,23 +18,5 @@ describe('Test list ordering', {retries: 0}, () => {
         jcontent.switchToStructuredView();
         jcontent.editComponentByText('Events');
         getComponentBySelector(Collapsible, '[data-sel-content-editor-fields-group="Content list & ordering"]').get().should('exist');
-    });
-
-    it('Activates and deactivates automatic ordering', () => {
-        jcontent = JContent.visit(siteKey, 'en', 'pages/home');
-        jcontent.switchToStructuredView();
-        contentEditor = jcontent.editComponentByText('landing');
-        cy.get('p[data-sel-field-picker-name="true"]').should('exist');
-        cy.get('input[id="jmix:orderedList"]').click({force: true});
-        contentEditor.save();
-
-        contentEditor = jcontent.editComponentByText('landing');
-        cy.get('p[data-sel-field-picker-name="true"]').should('not.exist');
-        cy.get('div[data-sel-content-editor-field="jmix:orderedList_firstField"]').should('exist');
-        cy.get('input[id="jmix:orderedList"]').click({force: true});
-        contentEditor.save();
-
-        contentEditor = jcontent.editComponentByText('landing');
-        cy.get('p[data-sel-field-picker-name="true"]').should('exist');
     });
 });
