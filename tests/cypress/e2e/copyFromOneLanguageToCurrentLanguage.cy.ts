7ea8d0956aa76f90d3ed5ca36f312b92dafb37bc
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-07-17 15:31:43 +0200

    lint --fix

--- tests/cypress/e2e/copyFromOneLanguageToCurrentLanguage.cy.ts
+++ tests/cypress/e2e/copyFromOneLanguageToCurrentLanguage.cy.ts
@@ -1,12 +1,10 @@
-import { addNode, createSite, createUser, deleteSite, getNodeByPath, grantRoles, setNodeProperty } from "@jahia/cypress"
-import { PickerTableRow } from "../page-object/pickerTable";
-
+import {addNode, createSite, createUser, deleteSite, getNodeByPath, grantRoles, setNodeProperty} from '@jahia/cypress';
 
 const TwoLanguagesSiteKey = 'severalLanguages';
 const OneLanguageSiteKey = 'oneLanguage';
 const editorLogin = {username: 'languagesEditor', password: 'password'};
 
-const addQaModuleToSite = (siteKey) => {
+const addQaModuleToSite = siteKey => {
     getNodeByPath('/sites/' + siteKey, ['j:installedModules']).its('data.jcr.nodeByPath.properties[0].values').as('installedModules');
 
     cy.get('@installedModules').then(result => {
@@ -17,29 +15,28 @@ const addQaModuleToSite = (siteKey) => {
         resultArr.push('qa-module');
         setNodeProperty('/sites/' + siteKey, 'j:installedModules', resultArr, 'en');
     });
-}
+};
 
 const checkFields = (siteKey, fieldValue, language) => {
     getNodeByPath(`/sites/${siteKey}/contents/all-fields`, ['smallText', 'textarea'], language).then(result => {
-        result = result.data.jcr.nodeByPath.properties
+        result = result.data.jcr.nodeByPath.properties;
 
         Cypress.$(result).each((index, property) => {
             expect(property.value).to.eq(fieldValue);
-        })
-    })
-}
+        });
+    });
+};
 
-const setCopyLanguage = (uuid) => {
+const setCopyLanguage = uuid => {
     cy.visit('/jahia/content-editor/fr/edit/' + uuid);
-    cy.get("button[data-sel-role='3dotsMenuAction']").click();
-    cy.get("[data-sel-role='jcontent-content-editor/header/3dots']").find("li:contains('Copy from languages')").click();
-    cy.get("div[role='document']").find("div[role='dropdown']").click();
-    cy.get("menu:contains('English')").find("li:contains('English')").click();
-}
+    cy.get('button[data-sel-role=\'3dotsMenuAction\']').click();
+    cy.get('[data-sel-role=\'jcontent-content-editor/header/3dots\']').find('li:contains(\'Copy from languages\')').click();
+    cy.get('div[role=\'document\']').find('div[role=\'dropdown\']').click();
+    cy.get('menu:contains(\'English\')').find('li:contains(\'English\')').click();
+};
 
-describe("test copyFromOneLanguageToCurrentLanguage", () => {
-
-    before("Create testsites", () => {
+describe('test copyFromOneLanguageToCurrentLanguage', () => {
+    before('Create testsites', () => {
         cy.login();
         createSite(TwoLanguagesSiteKey, {languages: 'en,fr', templateSet: 'dx-base-demo-template', serverName: 'localhost', locale: 'en'});
         createSite(OneLanguageSiteKey, {templateSet: 'qa-simpleTemplateSet', serverName: 'localhost', locale: 'en'});
@@ -51,62 +48,61 @@ describe("test copyFromOneLanguageToCurrentLanguage", () => {
         cy.logout();
     });
 
-    it("test", () => {
+    it('test', () => {
         cy.login(editorLogin.username, editorLogin.password);
 
-            addNode({
-                parentPathOrId: `/sites/${TwoLanguagesSiteKey}/contents`,
-                primaryNodeType: 'qant:allFields',
-                name: 'all-fields',
-                properties: [{name:'smallText', value: 'Text in English', language: 'en'}, {name: 'textarea', value: 'Text in English', language: 'en'}]
-            }).its('data.jcr.addNode.uuid').as('allFieldsUuid');
+        addNode({
+            parentPathOrId: `/sites/${TwoLanguagesSiteKey}/contents`,
+            primaryNodeType: 'qant:allFields',
+            name: 'all-fields',
+            properties: [{name: 'smallText', value: 'Text in English', language: 'en'}, {name: 'textarea', value: 'Text in English', language: 'en'}]
+        }).its('data.jcr.addNode.uuid').as('allFieldsUuid');
 
-            checkFields(TwoLanguagesSiteKey, 'Text in English', 'en');
+        checkFields(TwoLanguagesSiteKey, 'Text in English', 'en');
 
+        checkFields(TwoLanguagesSiteKey, '', 'fr');
+
+        cy.get('@allFieldsUuid').then(uuid => {
+            setCopyLanguage(uuid);
+            cy.get('div[role=\'document\']').find('button:contains(\'Cancel\')').click();
             checkFields(TwoLanguagesSiteKey, '', 'fr');
 
-            cy.get('@allFieldsUuid').then(uuid => {
+            setCopyLanguage(uuid);
+            cy.get('div[role=\'document\']').find('button:contains(\'Apply\')').click();
+            cy.get(('button[data-sel-role=\'submitSave\']')).click();
 
-                setCopyLanguage(uuid);
-                cy.get("div[role='document']").find("button:contains('Cancel')").click();
-                checkFields(TwoLanguagesSiteKey, '', 'fr');
+            checkFields(TwoLanguagesSiteKey, 'Text in English', 'fr');
+            setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'smallText', 'Text in French', 'fr');
+            setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'textarea', 'Text in French', 'fr');
+            checkFields(TwoLanguagesSiteKey, 'Text in French', 'fr');
 
-                setCopyLanguage(uuid);
-                cy.get("div[role='document']").find("button:contains('Apply')").click();
-                cy.get(("button[data-sel-role='submitSave']")).click();
+            setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'smallText', '', 'en');
+            setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'textarea', 'Other text in English', 'en');
 
-                checkFields(TwoLanguagesSiteKey, 'Text in English', 'fr');
-                setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'smallText', 'Text in French', 'fr');
-                setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'textarea', 'Text in French', 'fr');
-                checkFields(TwoLanguagesSiteKey, 'Text in French', 'fr');
+            setCopyLanguage(uuid);
+            cy.get('div[role=\'document\']').find('button:contains(\'Apply\')').click();
+            cy.get(('button[data-sel-role=\'submitSave\']')).click();
+        });
 
-                setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'smallText', '', 'en');
-                setNodeProperty(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, 'textarea', 'Other text in English', 'en');
+        getNodeByPath(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, ['smallText', 'textarea'], 'fr').then(result => {
+            result = result.data.jcr.nodeByPath.properties;
 
-                setCopyLanguage(uuid);
-                cy.get("div[role='document']").find("button:contains('Apply')").click();
-                cy.get(("button[data-sel-role='submitSave']")).click();
+            Cypress.$(result).each((index, property) => {
+                if (property.name === 'smallText') {
+                    expect(property.value).to.eq('Text in French');
+                }
 
-                getNodeByPath(`/sites/${TwoLanguagesSiteKey}/contents/all-fields`, ['smallText', 'textarea'], 'fr').then(result => {
-                    result = result.data.jcr.nodeByPath.properties
-            
-                    Cypress.$(result).each((index, property) => {
-                        if(property.name === 'smallText') {
-                            expect(property.value).to.eq('Text in French');
-                        }
-                        if(property.name === 'textarea') {
-                            expect(property.value).to.eq('Other text in English');
-                        }
-                    })
-                })
-            })
-            
+                if (property.name === 'textarea') {
+                    expect(property.value).to.eq('Other text in English');
+                }
+            });
+        });
 
         cy.logout();
     });
 
-    after("Delete testsites", () => {
+    after('Delete testsites', () => {
         deleteSite(TwoLanguagesSiteKey);
         deleteSite(OneLanguageSiteKey);
     });
-})
+});
