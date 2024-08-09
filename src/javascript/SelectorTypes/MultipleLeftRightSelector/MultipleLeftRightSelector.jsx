4ac98a83b23c1ae7234b926c2a8c4bab23fb4403
akarmanov <alexandrk_@hotmail.com>
2024-08-09 09:44:02 -0400

    QA-15247 Make sure deleted values don't break UI

--- src/javascript/SelectorTypes/MultipleLeftRightSelector/MultipleLeftRightSelector.jsx
+++ src/javascript/SelectorTypes/MultipleLeftRightSelector/MultipleLeftRightSelector.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import {FieldPropTypes} from '~/ContentEditor.proptypes';
 import {ListSelector} from '@jahia/moonstone';
@@ -6,26 +6,33 @@ import {useTranslation} from 'react-i18next';
 
 const toArray = value => (Array.isArray(value) ? value : [value]);
 
+const constraintsConsistentValues = (arrayValue, options) => {
+    const availableValues = options.map(valueConstraint => valueConstraint.value);
+    return arrayValue.filter(v => availableValues.includes(v));
+};
+
 export const MultipleLeftRightSelector = ({field, onChange, value}) => {
     const {t} = useTranslation('content-editor');
     const labelBase = 'label.contentEditor.picker.selectors.multipleLeftRightSelector';
-    const arrayValue = value ? toArray(value) : [];
-
-    // Reset selection if previously selected option no longer available
-    useEffect(() => {
-        if (arrayValue && arrayValue.length > 0) {
-            const availableValues = field.valueConstraints.map(valueConstraint => valueConstraint.value.string);
-            const actualValues = arrayValue.filter(v => availableValues.includes(v));
-            if (actualValues.length !== arrayValue.length) {
-                onChange(actualValues);
-            }
-        }
-    }, [value, onChange]); // eslint-disable-line
-
+    const arrayValue = useMemo(() => value ? toArray(value) : [], [value]);
     const options = field.valueConstraints.map(constraint => ({
         label: constraint.displayValue,
         value: constraint.value.string
     }));
+
+    // Reset selection if previously selected option no longer available
+    useEffect(() => {
+        if (arrayValue && arrayValue.length > 0) {
+            const actualValues = constraintsConsistentValues(arrayValue, options);
+            if (actualValues.length !== arrayValue.length) {
+                onChange(actualValues);
+            }
+        }
+    }, [value, arrayValue, onChange, options]);  // eslint-disable-line
+
+    if (arrayValue.length !== constraintsConsistentValues(arrayValue, options).length) {
+        return null;
+    }
 
     return (
         <ListSelector
